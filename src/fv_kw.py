a3b64acfe01556d309389823be7639bf5ee29264
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-15 11:14:32 +0200

    Add vx_litera

--- src/fv_kw.py
+++ src/fv_kw.py
@@ -5,3 +5,9 @@ import unittest
 
 def vx_porz(litera):
     return ord(litera.upper()) - ord('A') + 1
+
+def vx_litera(liczba):
+    if 1 <= liczba <= 26:
+        return chr(ord('A') + liczba - 1)
+    else:
+        raise RuntimeError('Poza zakresem?: %s' % repr(liczba))
