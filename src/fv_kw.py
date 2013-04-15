c3b5c0a18dae364f3c9710587995616aa8f2b426
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-15 12:41:01 +0200

    Add ColCalc

--- src/fv_kw.py
+++ src/fv_kw.py
@@ -33,6 +33,53 @@ def vx_rev_lt(liczba):
         wynik = vx_litera(liczba)
     return wynik
 
+class ColCalc:
+    def __init__(self, my_offset):
+        '''
+        ColCalc:
+        '''
+        self.my_offset = my_offset
+
+    def vx_porz(self, litera):
+        '''
+        ColCalc:
+        '''
+        return ord(litera.upper()) - ord('A') + 1
+
+    def vx_litera(self, liczba):
+        '''
+        ColCalc:
+        '''
+        if 1 <= liczba <= 26:
+            return chr(ord('A') + liczba - 1)
+        else:
+            raise RuntimeError('Poza zakresem?: %s' % repr(liczba))
+
+    def vx_lt(self, napis):
+        '''
+        ColCalc:
+        Numer kolejny litery, aby łatwiej wpisywać adresy w Excelu
+        '''
+        ile = len(napis)
+        if ile == 1:
+            wynik = vx_porz(napis)
+        elif ile == 2:
+            wynik = vx_porz(napis[0]) * 26 + vx_porz(napis[1])
+        else:
+            raise RuntimeError('Nieobslugiwany napis kolumny: %s' % repr(napis))
+        return wynik
+
+    def vx_rev_lt(self, liczba):
+        '''
+        ColCalc:
+        '''
+        if liczba > 26:
+            a, b = divmod(liczba - 1, 26)
+            wynik = vx_rev_lt(a) + vx_litera(b + 1)
+        else:
+            wynik = vx_litera(liczba)
+        return wynik
+
 class TestKolumnLiterowych(unittest.TestCase):
     def test_kolumn_literowych(self):
         '''
