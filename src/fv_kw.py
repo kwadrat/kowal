b6f42929d1af7f3c01cfec5ac5a162dcbeef3930
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-15 13:20:18 +0200

    Remove vx_lt

--- src/fv_kw.py
+++ src/fv_kw.py
@@ -12,19 +12,6 @@ def vx_litera(liczba):
     else:
         raise RuntimeError('Poza zakresem?: %s' % repr(liczba))
 
-def vx_lt(napis):
-    '''
-    Numer kolejny litery, aby łatwiej wpisywać adresy w Excelu
-    '''
-    ile = len(napis)
-    if ile == 1:
-        wynik = vx_porz(napis)
-    elif ile == 2:
-        wynik = vx_porz(napis[0]) * 26 + vx_porz(napis[1])
-    else:
-        raise RuntimeError('Nieobslugiwany napis kolumny: %s' % repr(napis))
-    return wynik
-
 class ColCalc:
     def __init__(self, my_offset):
         '''
@@ -82,12 +69,6 @@ class TestKolumnLiterowych(unittest.TestCase):
         '''
         self.assertEqual(vx_porz('A'), 1)
         self.assertEqual(vx_porz('Z'), 26)
-        self.assertEqual(vx_lt('A'), 1)
-        self.assertEqual(vx_lt('Z'), 26)
-        self.assertEqual(vx_lt('AA'), 27)
-        self.assertEqual(vx_lt('AB'), 28)
-        self.assertEqual(vx_lt('BA'), 53)
-        self.assertRaises(RuntimeError, vx_lt, 'AAA')
         self.assertRaises(RuntimeError, vx_litera, 0)
         self.assertRaises(RuntimeError, vx_litera, 27)
 
