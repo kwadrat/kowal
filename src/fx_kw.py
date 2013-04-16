83697a99feb03954f4d27a0f2c872748a18eabb2
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-17 00:43:35 +0200

    Add __repr__

--- src/fx_kw.py
+++ src/fx_kw.py
@@ -31,6 +31,12 @@ class HourServer:
         self.col_in_sheet = start_col + self.column_index
         self.header_for_hour_column = describe_column(column_index)
 
+    def __repr__(self):
+        '''
+        HourServer:
+        '''
+        return 'HS(%s)' % self.header_for_hour_column
+
 def prepare_time_headers(start_col):
     all_time_columns = []
     for column_index in xrange(24):
