95fcfa6df66060855ef4fe341e9f27c85815c084
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-17 03:47:16 +0200

    Rebuild enter_data

--- src/fu_kw.py
+++ src/fu_kw.py
@@ -170,6 +170,10 @@ class DataReader:
         '''
         DataReader:
         '''
+        for single_row in data_rows:
+            row_date = self.vx_t_date('A', single_row)
+            for single_column in data_headers:
+                self.fetch_field(dfb, key_object, single_row, row_date, single_column)
 
     def analyze_this_sheet(self, dfb):
         '''
