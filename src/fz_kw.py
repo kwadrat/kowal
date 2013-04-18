c58159cb7e482a63ca71c5b9a000018cef4efee5
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-18 16:02:05 +0200

    Use k_object

--- src/fz_kw.py
+++ src/fz_kw.py
@@ -86,6 +86,7 @@ def ptn_load_from_db(table_name):
         e_time=lc_kw.fq_m_time_qv,
         e_value=lc_kw.fq_m_value_qv,
         e_object=lc_kw.fq_f_object_qv,
+        k_object=lc_kw.fq_k_object_qv,
         )
 
 class TestVariousPatterns(unittest.TestCase):
