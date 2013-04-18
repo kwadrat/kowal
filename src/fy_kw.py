4e1984c9aad7ffcee63eb95b2bd04e4ab2a122f2
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-18 16:08:42 +0200

    Add lxa_15_inst

--- src/fy_kw.py
+++ src/fy_kw.py
@@ -30,3 +30,5 @@ lxa_13_inst = """\
 select uu_object.account,m_date,m_time,m_value from t,uu_object where t.f_object=uu_object.k_object;"""
 lxa_14_inst = """\
 select %(uu_object)s.%(account)s,%(e_date)s,%(e_time)s,%(e_value)s from %(table_name)s,%(uu_object)s where %(table_name)s.%(e_object)s=%(uu_object)s.%(k_object)s;"""
+lxa_15_inst = """\
+insert into uu_object (account) values ('n') returning k_object;"""
