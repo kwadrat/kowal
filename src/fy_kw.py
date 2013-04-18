5383dd65a92a29f104184b5d8becbb2590ea9d59
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-18 13:33:51 +0200

    Add lxa_9_inst

--- src/fy_kw.py
+++ src/fy_kw.py
@@ -18,3 +18,5 @@ lxa_7_inst = """\
 select k_object from uu_object where account='abc';"""
 lxa_8_inst = """\
 select %(k_object)s from %(uu_object)s where %(account)s='%(under_name)s';"""
+lxa_9_inst = """\
+select * from t where f_object=123 and m_date='2013-01-31' and m_time='23:34';"""
