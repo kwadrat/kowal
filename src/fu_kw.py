0c1b8592535bc23b9d0c22d7fb7dde6a41e81362
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-18 20:59:38 +0200

    Add protection against empty cells - daylight saving time change

--- src/fu_kw.py
+++ src/fu_kw.py
@@ -84,7 +84,8 @@ class EnergyReader(CommonReader):
         my_hour = single_column.canonical_hour
         if not le_kw.dq_entry_already_inserted(dfb, lc_kw.fq_uu_energy_qv, key_object, row_date, my_hour):
             value = self.vx_num_peek(single_column.col_in_sheet, single_row)
-            le_kw.dq_insert_energy_entry(dfb, lc_kw.fq_uu_energy_qv, key_object, row_date, my_hour, value)
+            if value:
+                le_kw.dq_insert_energy_entry(dfb, lc_kw.fq_uu_energy_qv, key_object, row_date, my_hour, value)
 
     def enter_data(self, dfb, key_object, data_headers, data_rows):
         '''
