ac6baca1150f31ae0bf3311d6b7fd109e704d900
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-18 15:24:52 +0200

    Add lxa_11_inst

--- src/fy_kw.py
+++ src/fy_kw.py
@@ -22,3 +22,5 @@ lxa_9_inst = """\
 select * from t where f_object=123 and m_date='2013-01-31' and m_time='23:34';"""
 lxa_10_inst = """\
 select * from %(n_table)s where %(e_object)s=%(f_object)d and %(e_date)s='%(m_date)s' and %(e_time)s='%(m_time)s';"""
+lxa_11_inst = """\
+insert into t (f_object, m_date, m_time, m_value) values (123, '2013-01-31', '23:34', 0.000000);"""
