9d51bbc876f7efb03c387eca998212054cc4c717
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-22 12:02:39 +0200

    Add for_storing

--- src/lm_kw.py
+++ src/lm_kw.py
@@ -12,6 +12,9 @@ def d2a(a):
     '''Decimal -> ASCII(kropka)'''
     return '%f' % a
 
+def for_storing(value):
+    return 'NULL'
+
 class TestPointNumbers(unittest.TestCase):
     def test_point_numbers(self):
         '''
@@ -19,3 +22,4 @@ class TestPointNumbers(unittest.TestCase):
         '''
         self.assertEqual(a2d('1.5'), decimal.Decimal('1.5'))
         self.assertEqual(d2a(decimal.Decimal('1.5')), '1.500000')
+        self.assertEqual(for_storing(None), 'NULL')
