568121d76ac78548d4984233fb021b36510dd27d
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-22 15:41:58 +0200

    Add fq_k_power_qv

--- src/lc_kw.py
+++ src/lc_kw.py
@@ -4,6 +4,7 @@
 fq_account_qv = 'account'
 fq_f_object_qv = 'f_object'
 fq_k_object_qv = 'k_object'
+fq_k_power_qv = 'k_power'
 fq_m_date_qv = 'm_date'
 fq_m_samples_qv = 'm_samples'
 fq_m_time_qv = 'm_time'
