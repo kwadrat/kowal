39ef5c37cd864ab58fd2f4f51e085c0138627283
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-22 10:39:29 +0200

    Add lm_kw.py

---
+++ src/lm_kw.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import unittest
+import decimal
+
+class TestPointNumbers(unittest.TestCase):
+    def test_point_numbers(self):
+        '''
+        TestPointNumbers:
+        '''
