97f83a5b4c7a398544b8bff864d09f17557149c2
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-22 12:12:23 +0200

    Add hj_kw.py

---
+++ src/hj_kw.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import unittest
+
+def Poprzecinkuj(lista):
+    '''Zwraca napisy połączone przecinkami
+    '''
+    return ','.join(lista)
+
+def ladnie_przecinkami(lista):
+    return ', '.join(lista)
+
+def Pokoniuguj(lista):
+    '''Zwraca napisy w koniunkcji logicznej
+    '''
+    return ' and\n'.join(lista)
+
+def make_alternatives(lista):
+    return ' OR '.join(lista)
+
+def condition_kv(key, value):
+    if value is None:
+        status = "%(key)s is null" % dict(key=key)
+    else:
+        status = "%(key)s = '%(value)s'" % dict(key=key, value=value)
+    return status
+
+def conditions_separately(klucze, slownik, ignorowane_pola=None):
+    lista = []
+    for klucz in klucze:
+        if ignorowane_pola is not None and klucz in ignorowane_pola:
+            pass # Na żądanie użytkownika pomijamy ten klucz
+        else:
+            wartosc = slownik[klucz]
+            lista.append(condition_kv(klucz, wartosc))
+    return lista
+
+def otocz_cudzyslowem(napis):
+    return '"%s"' % napis
+
+def ls_przec(*args):
+    return ladnie_przecinkami(args)
+
+class TestProcessingSQL(unittest.TestCase):
+    def test_processing_sql(self):
+        '''
+        TestProcessingSQL:
+        '''
+        self.assertEqual(conditions_separately(['a'], {'a': None}), ["a is null"])
+        self.assertEqual(conditions_separately(['b'], {'b': 45}), ["b = '45'"])
+        self.assertEqual(condition_kv('b', 45), "b = '45'")
+        self.assertEqual(condition_kv('c', None), "c is null")
+        self.assertEqual(make_alternatives(['a', 'b', 'c']), "a OR b OR c")
+        self.assertEqual(ladnie_przecinkami(['a', 'b', 'c']), "a, b, c")
+        self.assertEqual(ls_przec('a', 'b', 'c'), 'a, b, c')
