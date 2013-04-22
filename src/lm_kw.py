71172f8e4cab6507fb8620ae562a1136c722a048
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-22 11:38:06 +0200

    Add d2a

--- src/lm_kw.py
+++ src/lm_kw.py
@@ -8,9 +8,14 @@ def a2d(a):
     '''ASCII(kropka) -> Decimal'''
     return decimal.Decimal(a)
 
+def d2a(a):
+    '''Decimal -> ASCII(kropka)'''
+    return '%f' % a
+
 class TestPointNumbers(unittest.TestCase):
     def test_point_numbers(self):
         '''
         TestPointNumbers:
         '''
         self.assertEqual(a2d('1.5'), decimal.Decimal('1.5'))
+        self.assertEqual(d2a(decimal.Decimal('1.5')), '1.500000')
