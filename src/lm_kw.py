6451d30bbed909d92b09f9fc9ddd4b11fe10cf38
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-22 10:43:11 +0200

    Add a2d

--- src/lm_kw.py
+++ src/lm_kw.py
@@ -4,8 +4,13 @@
 import unittest
 import decimal
 
+def a2d(a):
+    '''ASCII(kropka) -> Decimal'''
+    return decimal.Decimal(a)
+
 class TestPointNumbers(unittest.TestCase):
     def test_point_numbers(self):
         '''
         TestPointNumbers:
         '''
+        self.assertEqual(a2d('1.5'), decimal.Decimal('1.5'))
