557175d05ea9add8133223672248babda51f28fe
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-23 11:33:41 +0200

    Add part_of_day_hs

--- src/lp_kw.py
+++ src/lp_kw.py
@@ -4,8 +4,13 @@
 import datetime
 import unittest
 
+def part_of_day_hs(par_h, par_m, par_s):
+    return datetime.time(par_h, par_m, par_s).strftime('%H:%M')
+
 class TestDateQuarters(unittest.TestCase):
     def test_date_quarters(self):
         '''
         TestDateQuarters:
         '''
+        self.assertEqual(part_of_day_hs(2, 45, 00), '02:45')
+
