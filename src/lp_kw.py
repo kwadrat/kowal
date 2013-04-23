61351d581d56be1c949cdcff349ba2caab0339c9
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-23 11:25:05 +0200

    Add TestDateQuarters

--- src/lp_kw.py
+++ src/lp_kw.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+
+import datetime
+import unittest
+
+class TestDateQuarters(unittest.TestCase):
+    def test_date_quarters(self):
+        '''
+        TestDateQuarters:
+        '''
