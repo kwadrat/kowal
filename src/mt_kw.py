92d238f07e08cee7148611d8cd27232207ef35ba
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-25 11:53:04 +0200

    Add quarter_server

--- src/mt_kw.py
+++ src/mt_kw.py
@@ -25,6 +25,7 @@ class PowerReader(CommonReader):
         PowerReader:
         '''
         CommonReader.__init__(self, 96, lc_kw.fq_uu_power_qv)
+        self.quarter_server = lp_kw.QuarterServer()
 
     def vx_th_date(self, my_col, my_row):
         '''
