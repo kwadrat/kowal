533391fc6f88c26ece1116c13799a4afe215f9f2
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-25 12:41:14 +0200

    Update init

--- src/lq_kw.py
+++ src/lq_kw.py
@@ -6,6 +6,8 @@ class SampleRow:
         '''
         SampleRow:
         '''
+        self.sample_key = None
+        self.list_of_samples = None
 
     def new_and_empty(self, cnt_per_day):
         '''
