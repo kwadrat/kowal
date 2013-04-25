280ad47c92d109417d1de3b0afae53fc1fee3aed
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-25 12:31:50 +0200

    Add SampleRow

--- src/lq_kw.py
+++ src/lq_kw.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+class SampleRow:
+    def new_and_empty(self, cnt_per_day):
+        '''
+        SampleRow:
+        '''
+        self.sample_key = None
+        self.list_of_samples = [None] * cnt_per_day
