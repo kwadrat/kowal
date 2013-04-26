d6b45afaaaf1231c7563e85da1d761b4a48f6c02
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-26 13:26:25 +0200

    Use list of samples

--- src/mu_kw.py
+++ src/mu_kw.py
@@ -84,7 +84,7 @@ class CommonReader:
         lp_kw.verify_for_equal(tmp_object, key_object)
         lp_kw.verify_for_equal(tmp_date, row_date)
         my_sample_row = lq_kw.SampleRow()
-        my_sample_row.fill_from_data(sample_key, sample_data)
+        my_sample_row.fill_from_data(sample_key, tmp_samples)
         self.internal_rows[local_key] = my_sample_row
 
     def prepare_local_copy_of_row(self, dfb, key_object, row_date):
