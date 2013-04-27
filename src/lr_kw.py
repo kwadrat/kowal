b91c7fd35231728efacf222d5bc183f181ca9e89
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-27 16:45:44 +0200

    Add generate_gnuplot_drawing

--- src/lr_kw.py
+++ src/lr_kw.py
@@ -13,3 +13,14 @@ for i in NazwyModulow:
             exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
         else:
             exec 'import %(modul)s' % dict(modul = i)
+
+def generate_gnuplot_drawing(dfb):
+    pytanie = "SELECT m_samples from uu_energy where f_object=1 and m_date >= '2013-03-11' and m_date < '2013-03-25' order by m_date;"
+    result = dfb.query_dct(pytanie, flg_nowy=1)
+    tmp_frags = []
+    for row_nr, row_data in enumerate(result):
+        for col_nr, value in enumerate(row_data[0]):
+            tmp_frags.append('%d %d %f\n' % (col_nr, row_nr, value))
+        tmp_frags.append('\n')
+    together = ''.join(tmp_frags)
+    sf_iw_kw.zapisz_jawnie('gen0', together)
