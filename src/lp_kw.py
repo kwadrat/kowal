86bc2eada6ba8ca2f3339225057433bf5622fcdf
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-29 12:53:08 +0200

    Remove prepare_time_headers

--- src/lp_kw.py
+++ src/lp_kw.py
@@ -60,12 +60,6 @@ class HourMiniServer:
         '''
         return 'HS(%s)' % self.header_for_hour_column
 
-def prepare_time_headers(start_col):
-    all_time_columns = []
-    for column_index in xrange(24):
-        all_time_columns.append(HourMiniServer(start_col, column_index))
-    return all_time_columns
-
 class HourServer:
     def __init__(self, start_col):
         '''
