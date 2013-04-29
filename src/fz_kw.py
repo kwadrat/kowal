c1f78afe3605caa0df489de333a1e742929a9fb9
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-29 11:50:46 +0200

    Remove m_time

--- src/fz_kw.py
+++ src/fz_kw.py
@@ -72,7 +72,6 @@ def ptn_load_from_db(table_name):
         uu_object=lc_kw.fq_uu_object_qv,
         account=lc_kw.fq_account_qv,
         e_date=lc_kw.fq_m_date_qv,
-        e_time=lc_kw.fq_m_time_qv,
         e_value=lc_kw.fq_m_value_qv,
         e_object=lc_kw.fq_f_object_qv,
         k_object=lc_kw.fq_k_object_qv,
