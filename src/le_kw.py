697ff9b65ca84b45648446ef14946378e85645f0
Piotr Kasprzyk <ciri@ciri.pl>
2013-04-30 10:23:34 +0200

    Use query_silent

--- src/le_kw.py
+++ src/le_kw.py
@@ -36,4 +36,4 @@ def dq_insert_vector_of_samples(dfb, n_table, key_object, row_date, all_samples)
 
 def dq_update_vector_of_samples(dfb, n_table, sample_key, key_object, row_date, all_samples):
     db_statement = fz_kw.ptn_update_vector_of_samples(n_table, sample_key, key_object, row_date, all_samples)
-    dfb.query_old_silent(db_statement)
+    dfb.query_silent(db_statement)
