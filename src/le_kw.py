c71372a20bedc3c5e395167a3996414369a366f8
Piotr Kasprzyk <ciri@ciri.pl>
2013-05-10 06:46:37 +0200

    Add id_obiekt

--- src/le_kw.py
+++ src/le_kw.py
@@ -42,7 +42,7 @@ def dq_load_one_vector_from_db(dfb, table_name, key_object, row_date):
     db_statement = fz_kw.ptn_load_one_vector_from_db(table_name, key_object, row_date)
     return dfb.query_dct(db_statement)
 
-def dq_liczniki_poboru_w_roku(dfb, table_name):
+def dq_liczniki_poboru_w_roku(dfb, table_name, id_obiekt):
     db_statement = fz_kw.ptn_liczniki_poboru_w_roku(table_name)
     return dfb.query_dct(db_statement)
 
