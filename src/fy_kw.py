20123dd452725b053b3749cf29387caeddffe80e
Piotr Kasprzyk <ciri@ciri.pl>
2013-05-17 08:29:53 +0200

    Rename conf_kw.py -> co_kw.py

--- src/fy_kw.py
+++ src/fy_kw.py
@@ -37,8 +37,8 @@ INSERT INTO %(n_table)s (%(e_object)s, %(e_date)s, %(e_samples)s) VALUES (%(f_ob
 lxa_19_inst = """\
 git checkout -b akw_2013.01.31_23.59.00; git checkout master
 cd ../dkw; git checkout -b akw_2013.01.31_23.59.00; git checkout master; cd ../bkw
-vi conf_kw.py
-rm ../ckw/src/*.py; cp ../dkw/src/*.py ../ckw/src/; rm ../akw/*.py; cp *.py ../akw/; git checkout conf_kw.py; touch ../akw/pw_kw.py
+vi co_kw.py
+rm ../ckw/src/*.py; cp ../dkw/src/*.py ../ckw/src/; rm ../akw/*.py; cp *.py ../akw/; git checkout co_kw.py; touch ../akw/pw_kw.py
 """
 lxa_20_inst = """\
 cd ..;tar cjf bkw_2013.01.31_23.59.00.tar.bz2 bkw dkw;mv bkw_2013.01.31_23.59.00.tar.bz2 bkw; cd bkw
