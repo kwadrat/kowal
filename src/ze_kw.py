aea6e02bc77de2281f5fa1265c32c30167c68934
Piotr Kasprzyk <ciri@ciri.pl>
2013-06-05 14:12:04 +0200

    Add ze_kw.py

---
+++ src/ze_kw.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''Atrybut 'title' w HTML'''
+
+import unittest
+
+def qh_ttl(tmp_title):
+    if tmp_title is None:
+        wynik = ''
+    else:
+        wynik = ' title="%(tmp_title)s"' % dict(tmp_title=tmp_title)
+    return wynik
+
+class TestTytuluHtml(unittest.TestCase):
+    def test_tytulu_html(self):
+        '''
+        TestTytuluHtml:
+        '''
+        self.assertEqual(qh_ttl('abc'), ' title="abc"')
+        self.assertEqual(qh_ttl(None), '')
