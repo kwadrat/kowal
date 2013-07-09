a81f77f0c93a7cbe67414fd014e2ac96b0c81f40
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-09 22:30:44 +0200

    Add ld_iw_kw.py

---
+++ src/ld_iw_kw.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+glowny_punkt_dla_zseu = '62470-003'
+szkielet_lodowiska = 'lodowisko %g m3, %s zł'
+
+# Numer punktu poboru wody dla lodowiska przy ZSEU
+wszystkie_pkty_lodowiska = frozenset(['62470-005', '62470-007', '62470-008'])
