fe03240dcc1ba546f84376f27bb011d84d640fa3
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-09 22:44:00 +0200

    Add we_nl_kw.py

---
+++ src/we_nl_kw.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+class WartosciExcel:
+    '''
+    Stałe wartości na podstawie:
+    http://nerds-central.blogspot.com/2007/02/excel-constant-definitions-for-vbscript.html
+    '''
+    # from enum XlWindowView
+    xlNormalView = 1
+    xlPageBreakPreview = 2
+
+    # from enum XlDirection
+    xlDown = -4121
+    xlToLeft = -4159
+    xlToRight = -4161
+    xlUp = -4162
