b84614ac98c5e87205515acacd6acddae711a672
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-09 22:25:27 +0200

    Add sa_kw.py

---
+++ src/sa_kw.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import locale
+import operator
+
+def UstawienieLocale():
+    NazwaLocale = "pl_PL.UTF-8"
+    locale.setlocale(locale.LC_COLLATE, NazwaLocale)
+    locale.setlocale(locale.LC_CTYPE, NazwaLocale)
+
+def poukladaj_wedlug_drugiego(przedzialy):
+    przedzialy.sort(key=operator.itemgetter(1), reverse = False)
