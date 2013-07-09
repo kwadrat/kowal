50cc215997bef5ee385a9f10a471dd563cf15028
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-09 22:47:22 +0200

    Add zt_kw.py

---
+++ src/zt_kw.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+class PorcjowanieLinii:
+    def __init__(self, liczba):
+        '''
+        PorcjowanieLinii:
+        '''
+        self.rh_liczba = liczba
+        self.zeruj()
+
+    def zeruj(self):
+        '''
+        PorcjowanieLinii:
+        '''
+        self.rh_licznik = 0
+
+    def paginate(self):
+        '''
+        PorcjowanieLinii:
+        '''
+        self.rh_licznik += 1
+        if self.rh_licznik >= self.rh_liczba:
+            self.zeruj()
+            raw_input('Press Enter: ')
