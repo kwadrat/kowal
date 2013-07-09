0517ab4125d4852dddec542e91866d6c0bb6ffe8
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-09 22:39:59 +0200

    Add sj_kw.py

---
+++ src/sj_kw.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'Jeden słupek do narysowania - współrzędne'
+
+class JedenSlupek:
+    def __init__(self, SlWspX, DolSlupka, GoraSlupka, Etykieta, Wartosc, jeden_odc_bzw):
+        '''
+        JedenSlupek:
+        '''
+        self.SlWspX = SlWspX
+        self.DolSlupka = DolSlupka
+        self.GoraSlupka = GoraSlupka
+        self.Etykieta = Etykieta
+        self.Wartosc = Wartosc
+        self.jeden_odc_bzw = jeden_odc_bzw
+
+    def zwroc_pelna_krotke(self):
+        '''
+        JedenSlupek:
+        '''
+        return (self.SlWspX, self.DolSlupka, self.GoraSlupka, self.Etykieta, self.Wartosc, self.jeden_odc_bzw)
+
+    def klucz_porzadku(self):
+        '''
+        JedenSlupek:
+        Sortowanie według góry słupka - te najwyższe (najmniejsza wsp. Y) są
+        rysowane najwcześniej
+        '''
+        return self.GoraSlupka
