3a3a6e35d75f84952b07654d34c3c7614387ff3b
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-09 22:30:02 +0200

    Add ust_iw_kw.py

---
+++ src/ust_iw_kw.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+TymczasowoM0 = 0
+TymczasowoM1 = 1
+
+# Tymczasowa zmienna, docelowo ma być zmieniona na 1,
+# gdy zostanie dopracowana weryfikacja faktur G-3
+# na kwotę 804.04 zł: 00/03836/2010, K80/00055/2010
+DocelowoNowyKodDlaKorekt = 1
+
+# Czy chcemy widzieć wizualizację faktury
+TymczasowoSzczegolowaWizualizacja = 0
+
+LokalnaDiagnostykaKlas = 0 and TymczasowoSzczegolowaWizualizacja
+
+# Wizualizacja działania nadliczników
+TymczasowoWizualizacjaNadlicznikow = 0 and TymczasowoSzczegolowaWizualizacja
+
+DocelowoZmieniajPochodneFaktury = 1 and DocelowoNowyKodDlaKorekt
+
+# Wizualizacja działania słowników zapalających licznikowane,
+# czyli dopasowania wody i ścieków
+TymczasowoWizualizacjaLicznikowanych = 0 and TymczasowoSzczegolowaWizualizacja
+
+# Włącz wizualizację dopasowywania faktur zwykłych i korygujących
+TymczasowoWizualizacjaZestawuFaktur = 1 and TymczasowoSzczegolowaWizualizacja
+
+# Pokaz wynikowych słowników
+WyswietlWynikoweSlowniki = 0
+
+# Pokaz zapytań SQL
+TymczasowoPokazSQL = 1
+
+# Pokazywanie słupków i tabelek (można wyłączyć, aby dostać same paski)
+DocelowoSlupki = 1
+DocelowoTabelki = 1
