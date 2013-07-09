967a687809c17f033e6137e6c04010a4e0cb8e0c
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-09 08:54:33 +0200

    Add dx_kw.py

---
+++ src/dx_kw.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+class KlasaBleduBazy:
+    def ustaw_przyczyne(self, napis):
+        '''
+        KlasaBleduBazy:
+        '''
+        self.napis_przyczyny = napis
+
+    def __init__(self):
+        '''
+        KlasaBleduBazy:
+        '''
+        self.ustaw_przyczyne('')
+
+    def last_error(self):
+        '''
+        KlasaBleduBazy:
+        '''
+        return self.napis_przyczyny
+
+egz_bbd = KlasaBleduBazy()
