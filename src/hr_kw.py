1adc9301a630420b0917d096a6cbdaa7305a5294
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-09 22:41:11 +0200

    Add hr_kw.py

---
+++ src/hr_kw.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+Prostokąt do narysowania na wykresie słupkowym, z możliwością zaznaczenia
+przekroczenie mocy (moc pobrana jest większa niż moc zamówiona)
+'''
+
+class ProstokatDoRysowania:
+    def __init__(self, x1, y1, x2, y2):
+        '''
+        ProstokatDoRysowania:
+        '''
+        self.wspolrzedne = (x1, y1, x2, y2)
+        self.przekroczenie_a_umw = 0
+
+    def zwroc_cztery_wsp(self):
+        '''
+        ProstokatDoRysowania:
+        '''
+        return self.wspolrzedne
+
+    def zaznacz_przekroczenie(self, przekroczenie_a_umw):
+        '''
+        ProstokatDoRysowania:
+        '''
+        self.przekroczenie_a_umw = przekroczenie_a_umw
