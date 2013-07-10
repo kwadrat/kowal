a1216bc2d5e27e9009cb06d514aa1024f597880c
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-10 16:59:39 +0200

    Add rp_kw.py

---
+++ src/rp_kw.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+import lk_kw
+import oa_kw
+import pt_kw
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+KlasaObrazu = pt_kw.KlasaObrazu
+
+class WykresRaportu(KlasaObrazu):
+    def __init__(self, tgk, aqr, dwk, linii):
+        KlasaObrazu.__init__(self, tgk, aqr, dwk, lk_kw.LITERA_WYKRES)
+        self.szerokosc_obrazu = oa_kw.sk_pelny_obraz
+        self.szerokosc_wykresu = self.szerokosc_obrazu - 10
+        self.wysokosc_obrazu = 200
+        self.wysokosc_linii = 12
+        self.wysokosc_obrazu = linii * self.wysokosc_linii + 10
