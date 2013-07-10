6fcdac7affa0c56f41f40ccc35e691ac6bd40cc3
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-10 16:02:03 +0200

    Import fy_kw

--- src/ze_kw.py
+++ src/ze_kw.py
@@ -3,6 +3,19 @@
 
 import unittest
 
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+import fy_kw
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
 formularz_1c_kon_tabeli = '</table>\n'
 formularz_67c_kon_wiersza = '</tr>\n'
 formularz_1c_kon_komorki = '</td>\n'
