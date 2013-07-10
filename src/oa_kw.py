58830a53005b3293b7c8945a76263a3d76e5b0cb
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-10 12:44:37 +0200

    Add poziomo_dla_dni

--- src/oa_kw.py
+++ src/oa_kw.py
@@ -19,9 +19,29 @@ for i in NazwyModulow:
 # zmieści się w poziomie na ekranie 1024x768
 sk_pelny_obraz = 1250
 
+def poziomo_dla_dni(min_domain, akt, max_domain, min_pixels, max_pixels):
+    return int(
+        min_pixels
+        +
+        (akt - min_domain)
+        *
+        (max_pixels - min_pixels)
+        /
+        (max_domain - min_domain)
+        )
+
 class TestRozmiaruObrazu(unittest.TestCase):
     def test_rozmiaru_obrazu(self):
         '''
         TestRozmiaruObrazu:
         '''
         self.assertEqual(sk_pelny_obraz, 1250)
+
+    def test_szkieletowego_datownika(self):
+        '''
+        TestRozmiaruObrazu:
+        '''
+        self.assertEqual(poziomo_dla_dni(1, 4, 4, 0, 3), 3)
+        self.assertEqual(poziomo_dla_dni(0, 0, 4, 1, 3), 1)
+        self.assertEqual(poziomo_dla_dni(0, 31 + 365 + 31 + 29, 31 + 365 + 31 + 29, 0, sk_pelny_obraz), sk_pelny_obraz)
+
