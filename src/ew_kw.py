57e1e6cc2907cc9f2b3239d40116d444b4c4a921
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-11 16:06:36 +0200

    Add ew_kw.py

---
+++ src/ew_kw.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import unittest
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+import rq_kw
+import ez_kw
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+KlasaSzkieletuDat = ez_kw.KlasaSzkieletuDat
+
+class SzkieletDatDlaFakturMiesLat(KlasaSzkieletuDat):
+    def __init__(self):
+        '''
+        SzkieletDatDlaFakturMiesLat:
+        '''
+        KlasaSzkieletuDat.__init__(self)
+
+    def mam_dat_na_dluzszy_rok(self):
+        '''
+        SzkieletDatDlaFakturMiesLat:
+        Górne, dłuższe kreski dla poszczególnych faktur (tylko ciągłych)
+        Liczba miesięcy dla ciągłych faktur miesięcznych
+        '''
+        return self.liczba_paskow() == rq_kw.RokDluzszy
