1c8439370c15226426a792070714d22c1fb93fef
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-16 07:28:36 +0200

    Add przypisz_dla_roku_szkielet

--- src/ew_kw.py
+++ src/ew_kw.py
@@ -5,6 +5,7 @@ import unittest
 
 NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
 import rq_kw
+import dn_kw
 import ez_kw
 '''.splitlines()]
 
@@ -34,9 +35,19 @@ class SzkieletDatDlaFakturMiesLat(KlasaSzkieletuDat):
         '''
         return self.liczba_paskow() == rq_kw.RokDluzszy
 
+    def przypisz_dla_roku_szkielet(self, wybrany_rok, rok_z_rozszerzeniem):
+        '''
+        SzkieletDatDlaFakturMiesLat:
+        '''
+        tmp_szkielet = dn_kw.daty_roku(wybrany_rok, rok_z_rozszerzeniem=0)
+        self.przypisz_szkielet(tmp_szkielet)
+
 class TestSzkieletuDatDlaFakturMiesLat(unittest.TestCase):
     def test_szkieletu_dat_dla_faktur_mies_lat(self):
         '''
         TestSzkieletuDatDlaFakturMiesLat:
         '''
         obk = SzkieletDatDlaFakturMiesLat()
+        obk.przypisz_dla_roku_szkielet(2012, rok_z_rozszerzeniem=0)
+        self.assertEqual(obk.szkielet_pocz, 15340)
+        self.assertEqual(obk.szkielet_kon, 15706)
