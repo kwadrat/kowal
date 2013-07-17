a321fe2498ccdf889d286d13df6220aafbdc96c4
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-17 22:10:30 +0200

    Use by_ten

--- src/oq_kw.py
+++ src/oq_kw.py
@@ -37,7 +37,7 @@ class ScaleAdvisor(object):
         '''
         self.my_value = my_value
         self.last_tick_value = int(self.my_value)
-        self.total_tick_periods = self.calculate_tick_periods(self.my_value)
+        self.total_tick_periods = self.calculate_tick_periods(by_ten(self.my_value, 0))
 
     def limit_tick_periods(self, min_tick_periods):
         '''
