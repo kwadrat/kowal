c66d5c32d544ee3a2e6090af15da45d27da1d187
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-18 12:51:02 +0200

    Add ei_kw.py

---
+++ src/ei_kw.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+'''
+Etykiety dla zmiennych sesji
+'''
+
+rjb_eon_zlgwn = 'jestem_zalogowany'
+rjb_eon_obiekt = 'przyp_obkt'
+# Pola formularza
+# Informacja o zalogowaniu (1) lub braku zalogowania (0) użytkownika
+LoginLogged = 'kw_zalogowany1'
+LoginName = 'kw_login_name'
+LoginPass = 'kw_login_pass'
+LoginPoziom = 'login_poziom'
+CzasPocztk = 'czas_poczatku'
+
+# Nazwy pól formularza
+NazwaOkres = 'okres'
+NazwaRok = 'rok'
+NazwaPoziom = 'poziom' # Poziom uprawnień użytkownika
+NazwaDomena = 'domena' # Pracujemy w złotówkach czy w jednostkach ilości towaru
+NazwaPrezentacja = 'prezentacja' # Sposób prezentacji danych: tabela i/lub wykres
+NazwaARok = 'prwrk'
+NazwaBRok = 'drgrk'
+NazwaRazem = 'razem'
+NazwaAktWsz = 'aktwnwsz'
+NazwaZuzycie = 'zuzycie'
+NazwaPobor = 'pobor'
+NazwaPobCzas = 'pobczas'
+NazwaData = 'data'
