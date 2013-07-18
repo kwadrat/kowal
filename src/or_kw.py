f3dd873f103f362156da41a04b81e3972c3c24ec
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-18 12:53:11 +0200

    Add or_kw.py

---
+++ src/or_kw.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+import lw_kw
+import sk_ht_kw
+import ei_kw
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+Skrawek = sk_ht_kw.Skrawek
+
+class SkrPoborowegoCzasu(Skrawek):
+    '''
+    Wybór okresu poboru:
+    - dzień
+    - tydzień
+    - miesiąc
+    '''
+    def __init__(self):
+        '''
+        SkrPoborowegoCzasu:
+        '''
+        Skrawek.__init__(self)
+        self.moje_pole = ei_kw.NazwaPobCzas
+
+    def zbierz_html(self, tgk, dfb):
+        '''
+        SkrPoborowegoCzasu:
+        '''
+        return sk_ht_kw.ListWyboruOgolna(tgk, self.moje_pole, lw_kw.DanePoborowegoCzasu)
