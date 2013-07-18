2934c2196be235d8eca5d803ff8326e759052028
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-18 12:55:26 +0200

    Add ok_ht_kw.py

---
+++ src/ok_ht_kw.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+import rq_kw
+import sk_ht_kw
+import ei_kw
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+Skrawek = sk_ht_kw.Skrawek
+
+class SkrOkres(Skrawek):
+    '''Wybór okresu:
+       - w latach (całe lata pokazujemy na wykresie)
+       - rok (tylko konkretny rok)
+       - Raport 1
+       - Raport 2
+    '''
+
+    def __init__(self):
+        '''
+        SkrOkres:
+        '''
+        Skrawek.__init__(self)
+        self.moje_pole = ei_kw.NazwaOkres
+
+    def zbierz_html(self, tgk, dfb):
+        '''
+        SkrOkres:
+        '''
+        return sk_ht_kw.ListWyboruOgolna(tgk, self.moje_pole, rq_kw.DaneOkresu)
