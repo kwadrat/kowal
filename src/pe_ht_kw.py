7033614c298057e8fc7c09e3c3bade7545baeb37
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-18 13:43:51 +0200

    Add pe_ht_kw.py

---
+++ src/pe_ht_kw.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+'''
+Udawanie żądania WWW podczas wykonywania testów
+'''
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+import oc_kw
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+class PseudoReq(object):
+    def get_remote_host(self):
+        return oc_kw.rjb_klnt_ip
+    def write(self, dane):
+        open('/tmp/wyjscie_z_aplikacji_kw.html', 'ab').write(dane)
