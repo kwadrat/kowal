66237c735fc3827434c17ac02a3d928674cb6f6b
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-19 17:40:08 +0200

    Add fq_kw.py

---
+++ src/fq_kw.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+import re
+import bz2
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+import sf_iw_kw
+import dn_kw
+import dg_nl_kw
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+Katalog_dokumentow = os.getcwd().decode('cp1250') + u'\\'
+faktura_bazowa_szkielet = 'Energia_szablon.xls'
+katalog_logu = Katalog_dokumentow + u'archiwum_aplikacji'
+zeszyt_pomiarow = u"ZST_zużycie_en_elektr_obiekty_obce.xls"
+
+def nazwa_pliku_faktury_bazowej():
+    return Katalog_dokumentow + faktura_bazowa_szkielet
+
+def dolacz_katalog(plik_dokumentu):
+    return Katalog_dokumentow + plik_dokumentu
+
+def zrob_kopie_pliku(nazwa_programu, przedrostek, prawie_rowne = 0):
+    if dg_nl_kw.TymczasowoF:
+        tmp_format = "nazwa_programu"; print 'Eval:', tmp_format, repr(eval(tmp_format))
+    koncowka_pliku = 'bz2'
+    kawalki_wzoru = [r'^', przedrostek, r'.+\.', koncowka_pliku, r'$']
+    poskladany_wzor = u''.join(kawalki_wzoru)
+    wzor_nazwy = re.compile(poskladany_wzor)
+    lista_nazw = os.listdir(katalog_logu)
+    zrob_kopie = 0
+    dane_programu = sf_iw_kw.wczytaj_plik(nazwa_programu)
+    lista_nazw = filter(lambda x: wzor_nazwy.search(x), lista_nazw)
+    if dg_nl_kw.TymczasowoF:
+        tmp_format = "lista_nazw"; print 'Eval:', tmp_format, repr(eval(tmp_format))
+    if lista_nazw:
+        dane_na_dysku = sf_iw_kw.wczytaj_plik(katalog_logu + u'\\' + lista_nazw[-1])
+        dane_rozpakowane = bz2.decompress(dane_na_dysku)
+        if prawie_rowne:
+            czy_rozne = dg_nl_kw.porownaj_wielkosc_i_kilkubajtowe_roznice(dane_programu, dane_rozpakowane)
+        else:
+            czy_rozne = (dane_programu != dane_rozpakowane)
+        if czy_rozne:
+            if dg_nl_kw.TymczasowoF:
+                print 'Robie kopie, bo dane sa rozne.'
+            zrob_kopie = 1
+    else:
+        if dg_nl_kw.TymczasowoF:
+            print 'Robie kopie, bo jej jeszcze nie ma.'
+        zrob_kopie = 1
+    if zrob_kopie:
+        dane_spakowane = bz2.compress(dane_programu)
+        elementy_nazwy = [u'\\', przedrostek, dn_kw.SekTeraz(), u'.', koncowka_pliku]
+        nazwa_pliku = u''.join(elementy_nazwy)
+        sf_iw_kw.zapisz_plik(katalog_logu + nazwa_pliku, dane_spakowane)
+
+def wykonaj_kopie_plikow():
+    sf_iw_kw.sprawdz_lub_utworz_katalog_logu(katalog_logu)
+    zrob_kopie_pliku(nazwa_pliku_faktury_bazowej(), 'kopia_szablon_', prawie_rowne = 1)
+    zrob_kopie_pliku(dolacz_katalog(zeszyt_pomiarow), 'kopia_pomiary_', prawie_rowne = 1)
