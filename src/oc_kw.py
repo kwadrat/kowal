947d196371bf114112b0ab2fbffd9465155a2a74
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-24 09:33:51 +0200

    Add rjb_tld_kw_f_apl

--- src/oc_kw.py
+++ src/oc_kw.py
@@ -40,6 +40,7 @@ rjb_hs_pcztk_sam = rjb_hs_pocz + adres_maszyny
 url_kotw_b_ica = url_kotw_a_ica + rjb_sam_slsh
 rjb_tld_kw_d_apl = 'inne'
 rjb_tld_kw_e_apl = '2'
+rjb_tld_kw_f_apl = rjb_tld_kw_d_apl + rjb_tld_kw_e_apl
 url_kotw_ica = url_kotw_b_ica + rjb_tld_kw_d_apl
 konto_uzytkownika = 'kwadrat'
 rjb_tld_kw_a_apl = rjb_sama_tylda + konto_uzytkownika
@@ -134,3 +135,4 @@ class TestConstantStrings(unittest.TestCase):
         self.assertEqual(fn_adres_post(0), '/inne2/l2.py')
         self.assertEqual(rjb_tld_kw_d_apl, 'inne')
         self.assertEqual(rjb_tld_kw_e_apl, '2')
+        self.assertEqual(rjb_tld_kw_f_apl, 'inne2')
