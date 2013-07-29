ecb7d742a9e2dfad095d75abaaee551cfd60566b
Piotr Kasprzyk <ciri@ciri.pl>
2013-07-29 17:35:22 +0200

    Reorder to none, zero, sum

--- src/fy_kw.py
+++ src/fy_kw.py
@@ -78,9 +78,9 @@ SELECT k_sample FROM uu_energy WHERE f_object=19 AND m_date='2013-02-01';"""
 lxa_37_inst = """\
 SELECT m_samples FROM uu_energy WHERE k_sample=1861;"""
 lxa_38_inst = """\
-SELECT m_date, m_samples, m_sum, m_zero, m_none FROM uu_energy WHERE f_object=7 ORDER BY m_date;"""
+SELECT m_date, m_samples, m_none, m_zero, m_sum FROM uu_energy WHERE f_object=7 ORDER BY m_date;"""
 lxa_39_inst = """\
-SELECT %(e_date)s, %(e_samples)s, %(e_sum)s, %(e_zero)s, %(e_none)s FROM %(table_name)s WHERE %(e_object)s=%(f_object)d ORDER BY %(e_date)s;"""
+SELECT %(e_date)s, %(e_samples)s, %(e_none)s, %(e_zero)s, %(e_sum)s FROM %(table_name)s WHERE %(e_object)s=%(f_object)d ORDER BY %(e_date)s;"""
 lxa_40_inst = "tabelkowiec"
 lxa_41_inst = """\
 SELECT k_sample FROM uu_energy WHERE f_object=19;"""
