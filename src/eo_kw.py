e832896d3efcb03d46685a14a8239188b6aaac90
Piotr Kasprzyk <ciri@ciri.pl>
2013-08-05 14:51:35 +0200

    Add eo_kw.py

---
+++ src/eo_kw.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+Identyfikatory obiektów i grup obiektów
+'''
+
+EtGrObPrzedsz = 'Przedszkola'
+GrOb_Przedszkole = 1
+GrOb_InneJedn = 9
+GrOb_Uczelnie = 10
+GrOb_SzkSp = 6
+DGN_Gr_Przedszkola = (str(GrOb_Przedszkole), EtGrObPrzedsz)
+
+Przedszkole1_x = 2
+Przedszkole7 = 9
+Przedszkole14 = 15
+Przedszkole20_x = 20
+Przedszkole21 = 21
+Przedszkole35_x = 29
+Przedszkole43 = 35
+BT_SP2_x = 38 # Szkoła podstawowa nr 2
+BT_SP3 = 39 # Szkoła podstawowa nr 3
+BT_SP20 = 51 # Szkoła podstawowa nr 13
+BT_SP22_x = 53 # Szkoła podstawowa nr 22
+Szsp_SOSW = 85
+PlywalniaPwst = 113
+JudoPwst = 115
+CKI_Pwst = 120
+ZST_Pwst = 83
+CKI_LabInf = 108
+BT_ZSzP3 = 62 # Zespół Szkolno-Przedszkolny nr 3
+BT_G11 = 71 # Gimnazjum nr 11
+BT_G13 = 73 # Gimnazjum nr 13
+BT_ZSB = 80 # Zespół Szkół Budowlanych
+BT_ZSEU = 81 # Zespół Szkół Ekonomiczno-Usługowych
+BT_ZS_1_Powst = 77 # Zespół Szkół nr 1 im. Powstańców Śląskich
+DGN_PrzedNr7 = (str(Przedszkole7), 'Przedszkole nr 7')
+
+Podlegle_pod_ZST = (PlywalniaPwst, JudoPwst, CKI_Pwst)
+przykladowa_fktr_p7_gj = 3133
+przykladowa_fktr_p21_wegiel = 409
+przykladowa_fktr_d_sosw = 364
+przykladowa_fktr_p_zst = 115
+przykladowa_fktr_u_p20_x = 83
+przykladowa_fktr_v_sp22_x = 3054
