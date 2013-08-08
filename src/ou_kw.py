cb00a64f89b8b4e49bf2b18d6e686fb48b5700e3
Piotr Kasprzyk <ciri@ciri.pl>
2013-08-08 10:51:02 +0200

    Move on_mouse

--- src/ou_kw.py
+++ src/ou_kw.py
@@ -48,12 +48,13 @@ class PomiarowySzeregListPoborow(OgolnySzeregListPoborow):
         self.przygotuj_sie_dla_listy_dni(lista_nr_probek)
         self.pobory_dla_licznikow()
 
-    def html_szeregu_poborow(self, on_mouse):
+    def html_szeregu_poborow(self):
         '''
         PomiarowySzeregListPoborow:
         '''
         lst_h = lh_kw.ListaHTML()
         lst_h.ddj(fy_kw.lxa_47_inst)
         self.przygotuj_dla_poborow()
+        on_mouse = {}
         self.grafika_poborow_dla_pomiarow(lst_h, on_mouse)
         return lst_h.polacz_html()
