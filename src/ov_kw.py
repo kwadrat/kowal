1d7cf81f571467f4cb8807988100b068a671f117
Piotr Kasprzyk <ciri@ciri.pl>
2013-08-08 09:24:05 +0200

    Add ov_kw.py

---
+++ src/ov_kw.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+'''
+Analiza poboru - pokryciowy szereg list
+'''
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+import lt_kw
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+OgolnySzeregListPoborow = lt_kw.OgolnySzeregListPoborow
+
+class PokryciowySzeregListPoborow(OgolnySzeregListPoborow):
+    def __init__(self, tgk, dfb):
+        '''
+        PokryciowySzeregListPoborow:
+        '''
+        OgolnySzeregListPoborow.__init__(tgk, dfb)
