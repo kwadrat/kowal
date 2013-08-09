e92537311079012bf40453cbbdc314775a0c1569
Piotr Kasprzyk <ciri@ciri.pl>
2013-08-09 12:11:52 +0200

    Reduce przygotuj_dla_pokrycia_poborow

--- src/ov_kw.py
+++ src/ov_kw.py
@@ -47,6 +47,6 @@ class PokryciowySzeregListPoborow(OgolnySzeregListPoborow):
         PokryciowySzeregListPoborow:
         '''
         lst_h = lh_kw.ListaHTML()
-        self.przygotuj_dla_pokrycia_poborow()
+        self.zapamietaj_wybory_formularza_poborow()
         self.wizualizacja_pokrycia_poborami(lst_h)
         return lst_h.polacz_html()
