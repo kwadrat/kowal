7a0c05d83449fe861abc5e6dc2e9a1eadf69b83a
Piotr Kasprzyk <ciri@ciri.pl>
2013-08-10 09:01:28 +0200

    Add wstawka_sql_liczba

--- src/ze_kw.py
+++ src/ze_kw.py
@@ -53,6 +53,13 @@ def wstawka_liczba(nazwa, liczba):
         napis = ' %s="%d"' % (nazwa, liczba)
     return napis
 
+def wstawka_sql_liczba(nazwa, liczba):
+    if liczba is None:
+        napis = ''
+    else:
+        napis = ' %s=%d' % (nazwa, liczba)
+    return napis
+
 def wyznacz_wstawke(nazwa, wartosc):
     if wartosc is None:
         napis = ''
@@ -232,6 +239,7 @@ class TestTytuluHtml(unittest.TestCase):
         self.assertEqual(qh_ttl('abc'), ' title="abc"')
         self.assertEqual(qh_ttl(None), '')
         self.assertEqual(wstawka_liczba('abc', 7), ' abc="7"')
+        self.assertEqual(wstawka_sql_liczba('abc', 7), ' abc=7')
         self.assertEqual(op_td(), '<td>')
         self.assertEqual(op_td(class_ = 'klasa_css'), '<td class="klasa_css">')
         self.assertEqual(op_td(class_ = 'klasa_css', colspan=2, rowspan=3), '<td class="klasa_css" colspan="2" rowspan="3">')
