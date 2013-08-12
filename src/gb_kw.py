4bd7356239fe456bf2c15b486ef4162a04bef28c
Piotr Kasprzyk <ciri@ciri.pl>
2013-08-12 14:17:43 +0200

    Add TestUnitNames

---
+++ src/gb_kw.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+'''
+Nazwy jednostek
+'''
+
+import unittest
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+class TestUnitNames(unittest.TestCase):
+    def test_unit_names(self):
+        '''
+        TestUnitNames:
+        '''
