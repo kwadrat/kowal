e22c7427085ad2bfc9c41b6e3adb3d70c08f13d2
Piotr Kasprzyk <ciri@ciri.pl>
2013-08-13 08:44:17 +0200

    Add krt_pobor

--- src/ou_kw.py
+++ src/ou_kw.py
@@ -25,7 +25,7 @@ for i in NazwyModulow:
 OgolnySzeregListPoborow = lt_kw.OgolnySzeregListPoborow
 
 class PomiarowySzeregListPoborow(OgolnySzeregListPoborow):
-    def __init__(self, tgk, dfb):
+    def __init__(self, tgk, dfb, krt_pobor):
         '''
         PomiarowySzeregListPoborow:
         '''
