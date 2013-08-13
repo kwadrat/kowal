7ba3605eb28b5f6895b0d361b5f8a6533ca0d4bf
Piotr Kasprzyk <ciri@ciri.pl>
2013-08-13 07:28:57 +0200

    Add krt_etykieta

--- src/dd_kw.py
+++ src/dd_kw.py
@@ -7,6 +7,7 @@ Cechy energii pobranej - energia, moc
 import unittest
 
 NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+import fy_kw
 import lc_kw
 import gb_kw
 import lw_kw
@@ -26,11 +27,13 @@ energy_chooser = {
         24,
         lc_kw.fq_uu_energy_qv,
         gb_kw.Jedn_kWh,
+        fy_kw.lxa_56_inst,
         ],
     lw_kw.Dn_Power: [
         96,
         lc_kw.fq_uu_power_qv,
         gb_kw.Jedn_kWtow,
+        fy_kw.lxa_56_inst,
         ],
     }
 
@@ -44,6 +47,7 @@ class CechaEnergii(object):
             self.krt_wymiar,
             self.krt_table,
             self.krt_jedn,
+            self.krt_etykieta,
             ) = energy_chooser[self.tvk_pobor]
 
 class TestEnergyFeatures(unittest.TestCase):
@@ -55,6 +59,7 @@ class TestEnergyFeatures(unittest.TestCase):
         self.assertEqual(obk.krt_wymiar, 24)
         self.assertEqual(obk.krt_table, lc_kw.fq_uu_energy_qv)
         self.assertEqual(obk.krt_jedn, gb_kw.Jedn_kWh)
+        self.assertEqual(obk.krt_etykieta, fy_kw.lxa_56_inst)
 
     def test_2_energy_features(self):
         '''
@@ -64,3 +69,4 @@ class TestEnergyFeatures(unittest.TestCase):
         self.assertEqual(obk.krt_wymiar, 96)
         self.assertEqual(obk.krt_table, lc_kw.fq_uu_power_qv)
         self.assertEqual(obk.krt_jedn, gb_kw.Jedn_kWtow)
+        self.assertEqual(obk.krt_etykieta, fy_kw.lxa_56_inst)
