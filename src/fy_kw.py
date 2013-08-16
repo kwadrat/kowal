3e94f86718ba5a381a8f3a594a4966727363e54e
Piotr Kasprzyk <ciri@ciri.pl>
2013-08-16 22:39:52 +0200

    Remove lxa_34_inst

--- src/fy_kw.py
+++ src/fy_kw.py
@@ -65,8 +65,6 @@ lxa_31_inst = """\
 SELECT %(e_samples)s FROM %(table_name)s WHERE %(e_object)s=%(f_object)d AND %(e_date)s='%(m_date)s';"""
 lxa_33_inst = """\
 SELECT m_samples FROM uu_power WHERE f_object=18 AND m_date = '2013-01-31' ORDER BY m_date;"""
-lxa_34_inst = """\
-SELECT m_samples FROM %(table_name)s WHERE k_sample=%(nr_probki)d;"""
 lxa_35_inst = """\
 SELECT m_samples FROM uu_power WHERE k_sample=1860;"""
 lxa_36_inst = """\
