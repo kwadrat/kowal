73d80b56313bcd543ebc99e6b09403bb3208b203
Piotr Kasprzyk <ciri@ciri.pl>
2013-08-17 18:26:48 +0200

    Import rq_kw

--- src/lm_kw.py
+++ src/lm_kw.py
@@ -4,6 +4,19 @@
 import unittest
 import decimal
 
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+import rq_kw
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
 def a2d(a):
     '''ASCII(kropka) -> Decimal'''
     return decimal.Decimal(a)
