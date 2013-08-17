9773e7a65239c187df5a38dc9bf4901ce844664a
Piotr Kasprzyk <ciri@ciri.pl>
2013-08-17 11:18:14 +0200

    Use SzkieletMiesiecznyDlaPoborow

--- src/lu_kw.py
+++ src/lu_kw.py
@@ -5,8 +5,8 @@ Analiza poboru - pomiarowy szereg list dla miesięcy w ciągu roku
 '''
 
 NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
-import dn_kw
 import lh_kw
+import ox_kw
 import lt_kw
 '''.splitlines()]
 
@@ -26,10 +26,7 @@ class PomiaryPoborowMiesiecznie(OgolnySzeregListPoborow):
         '''
         PomiaryPoborowMiesiecznie:
         '''
-        tvk_data = tgk.wez_date()
-        my_year, my_month = dn_kw.rok_mies_z_napisu(tvk_data)
-        my_start_day, my_end_day = dn_kw.ZakresMiesiaca(my_year, my_month)
-        aqr = None
+        aqr = ox_kw.SzkieletMiesiecznyDlaPoborow(krt_pobor)
         OgolnySzeregListPoborow.__init__(self, tgk, aqr, dfb, krt_pobor)
 
     def html_szeregu_poborow(self, krt_pobor):
