cd6aa49ae8392fc0a49100bcc0acdc94e5ecddf1
Piotr Kasprzyk <ciri@ciri.pl>
2013-08-17 10:59:38 +0200

    Add lu_kw.py

---
+++ src/lu_kw.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+'''
+Analiza poboru - pomiarowy szereg list dla miesięcy w ciągu roku
+'''
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+import lh_kw
+import lt_kw
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+OgolnySzeregListPoborow = lt_kw.OgolnySzeregListPoborow
+
+class PomiaryPoborowMiesiecznie(OgolnySzeregListPoborow):
+    def __init__(self, tgk, dfb, krt_pobor):
+        '''
+        PomiaryPoborowMiesiecznie:
+        '''
+        aqr = None
+        OgolnySzeregListPoborow.__init__(self, tgk, aqr, dfb, krt_pobor)
+
+    def html_szeregu_poborow(self, krt_pobor):
+        '''
+        PomiaryPoborowMiesiecznie:
+        '''
+        lst_h = lh_kw.ListaHTML()
+        return lst_h.polacz_html()
