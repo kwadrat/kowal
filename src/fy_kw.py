b3757a03e97b1b4e8d33684852b6eb9b17ac0d77
Piotr Kasprzyk <ciri@ciri.pl>
2013-08-17 13:17:54 +0200

    Remove lxa_10_inst

--- src/fy_kw.py
+++ src/fy_kw.py
@@ -20,8 +20,6 @@ lxa_8_inst = """\
 SELECT %(k_object)s FROM %(uu_object)s WHERE %(account)s='%(under_name)s';"""
 lxa_9_inst = """\
 SELECT k_sample, f_object, m_date, m_samples FROM uu_power WHERE f_object=123 AND m_date = '2013-01-31' ORDER BY m_date;"""
-lxa_10_inst = """\
-SELECT %(e_fields)s FROM %(n_table)s WHERE %(e_object)s=%(f_object)d AND %(e_date)s = '%(m_date)s' ORDER BY m_date;"""
 lxa_13_inst = """\
 SELECT uu_object.account,m_date,m_samples FROM uu_power,uu_object WHERE uu_power.f_object=uu_object.k_object;"""
 lxa_14_inst = """\
