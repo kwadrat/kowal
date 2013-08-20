fb5b10896fc724005909d77d4bc7ecdc4602d88a
Piotr Kasprzyk <ciri@ciri.pl>
2013-08-21 00:10:28 +0200

    Add rm_kw.py

---
+++ src/rm_kw.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+import fy_kw
+import ze_kw
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+class FormularzBazowySkrawkow(object):
+    def __init__(self, strona_bazowa):
+        '''
+        FormularzBazowySkrawkow:
+        '''
+        self.strona_bazowa = strona_bazowa
+
+    def zbierz_2html(self, tgk, dfb):
+        '''
+        FormularzBazowySkrawkow:
+        Wartość zwracana:
+        - lista kawałków strony HTML
+        '''
+        w = []
+
+        w.append(ze_kw.op_fmd(adres=self.strona_bazowa, name=fy_kw.lxa_43_inst))
+        for elem in self.moj_form:
+            w.append(elem.zbierz_html(tgk, dfb))
+        w.append(ze_kw.formularz_1c_kon_formularza)
+        return w
+
+    def get_fr(self, tgk, klucz):
+        '''
+        FormularzBazowySkrawkow:
+        '''
+        for elem in self.moj_form:
+            if elem.moje_pole == klucz:
+                return elem.pobierz_wartosc(tgk)
+        raise RuntimeError('Nieznany klucz: %s %s' % (repr(klucz), repr(self.moj_form)))
+
+    def mamy_wspolny_komplet_pol(self, tgk, dfb):
+        '''
+        FormularzBazowySkrawkow:
+        Wartość zwrotna (logiczna) mówi, czy mamy komplet wszystkich
+        potrzebnych danych z formularza
+        '''
+        nasz_zbior = set() # Zbiór nazw wypełnionych pól
+        for elem in self.moj_form:
+            wynik = elem.analiza_parametrow(tgk, dfb)
+            # Dopiero po analizie pole wartości będzie wypełnione
+            # i dopiero teraz można pobrać wartość pola
+            if wynik: # Sprawdź, czy mieliśmy to pole w formularzu
+                nasz_zbior.add(elem.moje_pole) # Dostaliśmy wartość tego pola
+        brakujace_pola = self.potrzebne_mi_pola - nasz_zbior
+        return not brakujace_pola
