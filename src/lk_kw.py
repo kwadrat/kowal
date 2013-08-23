1d35b97cb5a3ff69414490e0c3b1d387bef3f26c
Piotr Kasprzyk <ciri@ciri.pl>
2013-08-23 10:41:27 +0200

    Add TestSomeConstants

--- src/lk_kw.py
+++ src/lk_kw.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+import unittest
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
 PrawaDostepuPliku = 0777
 CHC_NO = 0
 CHC_YES = 1
@@ -10,3 +24,9 @@ LITERA_WYKRES = 'w'
 KLD_CIEMNY = 0
 KLD_JASNY = 1
 EtykietaLP = 'L.p.' # Liczba porządkowa
+
+class TestSomeConstants(unittest.TestCase):
+    def test_some_constants(self):
+        '''
+        TestSomeConstants:
+        '''
