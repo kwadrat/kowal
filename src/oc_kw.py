05113941bd3c511b9b6ca65b6b7ba7bf1761df5f
Piotr Kasprzyk <ciri@ciri.pl>
2013-08-26 18:32:42 +0200

    Add rjb_strona_jedenasta

--- src/oc_kw.py
+++ src/oc_kw.py
@@ -93,6 +93,7 @@ rjb_strona_siodma = KalejdoskopStron(7)
 rjb_strona_osma = KalejdoskopStron(8)
 rjb_strona_dziewiata = KalejdoskopStron(9)
 rjb_strona_dziesiata = KalejdoskopStron(10)
+rjb_strona_jedenasta = KalejdoskopStron(11)
 
 def fn_adres_post(wersja_produkcyjna):
     return lk_kw.rjb_sam_slsh + fn_a_in_dwa(wersja_produkcyjna) + lk_kw.rjb_sam_slsh + dodaj_py(rjb_strona_druga.rj_sam_rdzen)
@@ -146,6 +147,8 @@ class TestConstantStrings(unittest.TestCase):
         self.assertEqual(rjb_strona_dziewiata.rj_py_wersja, 'l9.py')
         self.assertEqual(rjb_strona_dziesiata.rj_sam_rdzen, 'l10')
         self.assertEqual(rjb_strona_dziesiata.rj_py_wersja, 'l10.py')
+        self.assertEqual(rjb_strona_jedenasta.rj_sam_rdzen, 'l11')
+        self.assertEqual(rjb_strona_jedenasta.rj_py_wersja, 'l11.py')
         self.assertEqual(fn_a_in_dwa(0), 'inne2')
         self.assertEqual(fn_a_in_dwa(1), 'inne')
         self.assertEqual(fn_adres_post(1), '/inne/l2.py')
