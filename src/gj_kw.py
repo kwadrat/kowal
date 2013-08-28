5cc8aca9ea405e2b6a01f4e6fa3a0558a0dcf948
Piotr Kasprzyk <ciri@ciri.pl>
2013-08-28 13:03:55 +0200

    Add gj_kw.py

---
+++ src/gj_kw.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import unittest
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+class BuildingIndicator(object):
+    def __init__(self, lokalny_the_building):
+        '''
+        BuildingIndicator:
+        '''
+        self.lokalny_the_building = lokalny_the_building
+        self.lokalny_mam_realny_obiekt = self.lokalny_the_building is not None
+
+class TestOpisuBudynku(unittest.TestCase):
+    def test_opisu_a_budynku(self):
+        '''
+        TestOpisuBudynku:
+        '''
+        lokalny_the_building = None
+        obk = BuildingIndicator(lokalny_the_building)
+        self.assertEqual(obk.lokalny_the_building, None)
+        self.assertEqual(obk.lokalny_mam_realny_obiekt, 0)
+
+    def test_opisu_b_budynku(self):
+        '''
+        TestOpisuBudynku:
+        '''
+        lokalny_the_building = 4
+        obk = BuildingIndicator(lokalny_the_building)
+        self.assertEqual(obk.lokalny_the_building, 4)
+        self.assertEqual(obk.lokalny_mam_realny_obiekt, 1)
+
