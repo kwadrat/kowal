e4b2e90663ba241c0a266c55315f9e736bb42e09
Piotr Kasprzyk <ciri@ciri.pl>
2013-09-04 08:39:40 +0200

    Import hj_kw

--- src/fv_kw.py
+++ src/fv_kw.py
@@ -3,6 +3,19 @@
 
 import unittest
 
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+import hj_kw
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
 class ColCalc(object):
     def __init__(self, my_offset):
         '''
