8217ca0d62dcc4bdf8a184ebc25f2391480e0490
Piotr Kasprzyk <ciri@ciri.pl>
2013-09-22 10:36:05 +0200

    Add num_format_str

--- src/la_kw.py
+++ src/la_kw.py
@@ -39,7 +39,7 @@ def analyze_excel_files(dfb, worker_class, filenames):
         obk.analyze_this_file(dfb, xlrd, single_file)
 
 class WriterGateway(object):
-    def prepare_font_size(self, the_size=None, bold=0):
+    def prepare_font_size(self, the_size=None, bold=0, num_format_str=None):
         '''
         WriterGateway:
         '''
@@ -49,6 +49,8 @@ class WriterGateway(object):
             the_font.height = the_size * 20 # Arial "the_size" pt
         the_font.bold = bold
         the_style.font = the_font
+        if num_format_str is not None:
+            the_style.num_format_str = num_format_str
         return the_style
 
     def __init__(self):
