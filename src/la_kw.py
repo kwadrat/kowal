c862e36741ffa356f9efe37058fb372695893362
Piotr Kasprzyk <ciri@ciri.pl>
2013-09-22 09:31:26 +0200

    Add prepare_font_size

--- src/la_kw.py
+++ src/la_kw.py
@@ -39,6 +39,16 @@ def analyze_excel_files(dfb, worker_class, filenames):
         obk.analyze_this_file(dfb, xlrd, single_file)
 
 class WriterGateway(object):
+    def prepare_font_size(self, the_size):
+        '''
+        WriterGateway:
+        '''
+        the_style = self.xlwt.XFStyle()
+        the_font = self.xlwt.Font()
+        the_font.height = the_size * 20 # Arial "the_size" pt
+        the_style.font = the_font
+        return the_style
+
     def __init__(self):
         '''
         WriterGateway:
