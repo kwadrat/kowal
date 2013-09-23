c5bc0b9f40dd929f495b9b52dcd8ecb4979ffae5
Piotr Kasprzyk <ciri@ciri.pl>
2013-09-23 10:43:54 +0200

    Add zapisz_swobodne_polaczone_num_komorki

--- src/la_kw.py
+++ src/la_kw.py
@@ -131,6 +131,15 @@ class WriterGateway(object):
         c2 = akt_kolumna + ile_w_poziomie
         self.sheet.write_merge(r1, r2, c1, c2, en_kw.utf_to_unicode(napis))
 
+    def zapisz_swobodne_polaczone_num_komorki(self, r1, r2, c1, c2, napis, style=None):
+        '''
+        WriterGateway:
+        '''
+        dc_style = {}
+        if style is not None:
+            dc_style['style'] = style
+        self.sheet.write_merge(r1, r2, c1, c2, napis, **dc_style)
+
     def zapisz_swobodne_polaczone_komorki(self, r1, r2, c1, c2, napis, style=None):
         '''
         WriterGateway:
