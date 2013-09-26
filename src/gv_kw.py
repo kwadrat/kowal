ca10e7f3100f65f550264d83fe2903d52127e2b0
Piotr Kasprzyk <ciri@ciri.pl>
2013-09-26 05:27:40 +0200

    Add gv_kw.py

---
+++ src/gv_kw.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+Rich (value, digits after comma, colour) numer
+'''
+
+import unittest
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+class RichNumber(object):
+    def __init__(self, rn_value, rn_after=None, rn_colour=None):
+        '''
+        RichNumber:
+        '''
+        self.rn_value = rn_value
+        self.rn_after = rn_after
+        self.rn_colour = rn_colour
+
+class TestTheNumber(unittest.TestCase):
+    def test_the_number(self):
+        '''
+        TestTheNumber:
+        '''
+        obk = RichNumber(0, rn_after=2, rn_colour='red')
