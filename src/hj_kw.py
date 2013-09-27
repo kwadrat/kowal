da25baca9e1f91d6126963f08d52b73c203ecf2d
Piotr Kasprzyk <ciri@ciri.pl>
2013-09-27 09:29:02 +0200

    Switch to tuples

--- src/hj_kw.py
+++ src/hj_kw.py
@@ -136,10 +136,10 @@ def ogranicz_wartosci_umowne(tmp_list):
 
 def rc_rozszczep(the_label):
     return {
-        'A1': ['A', 1],
-        'B1': ['B', 1],
-        'B2': ['B', 2],
-        'AB234': ['AB', 234],
+        'A1': ('A', 1),
+        'B1': ('B', 1),
+        'B2': ('B', 2),
+        'AB234': ('AB', 234),
     }[the_label]
 
 class TestProcessingSQL(unittest.TestCase):
@@ -188,7 +188,7 @@ class TestProcessingSQL(unittest.TestCase):
         self.assertEqual(ogranicz_wartosci_umowne([40.0, 40.0, 0.0]), [40.0])
         self.assertEqual(rcp_minus('A1', 'B1'), 'A1-B1')
         self.assertEqual(rcp_dziel('A1', 'B1'), 'A1/B1')
-        self.assertEqual(rc_rozszczep('A1'), ['A', 1])
-        self.assertEqual(rc_rozszczep('B1'), ['B', 1])
-        self.assertEqual(rc_rozszczep('B2'), ['B', 2])
-        self.assertEqual(rc_rozszczep('AB234'), ['AB', 234])
+        self.assertEqual(rc_rozszczep('A1'), ('A', 1))
+        self.assertEqual(rc_rozszczep('B1'), ('B', 1))
+        self.assertEqual(rc_rozszczep('B2'), ('B', 2))
+        self.assertEqual(rc_rozszczep('AB234'), ('AB', 234))
