bb87484c674396df6dabed3164636bc8a3388047
Piotr Kasprzyk <ciri@ciri.pl>
2013-09-30 12:25:13 +0200

    Add prefix

--- src/sf_iw_kw.py
+++ src/sf_iw_kw.py
@@ -104,8 +104,10 @@ def zapisz_plik(nazwa, dane, verbose = 0, mode = None):
     fd.write(dane)
     domknij_zapisany_plik(nazwa, fd, verbose, mode)
 
-def zapisz_ladnie(nazwa, dane, verbose=0, mode=None):
+def zapisz_ladnie(nazwa, dane, verbose=0, mode=None, prefix=None):
     fd = otworz_do_zapisu(nazwa)
+    if prefix is not None:
+        fd.write(prefix)
     pprint.pprint(dane, stream=fd)
     domknij_zapisany_plik(nazwa, fd, verbose, mode)
 
