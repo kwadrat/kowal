f4c7191d3fbf23fee216807688cd1d02c716faca
Piotr Kasprzyk <ciri@ciri.pl>
2013-10-07 12:56:57 +0200

    Move zapisz_direct

--- src/la_kw.py
+++ src/la_kw.py
@@ -200,6 +200,13 @@ class WriterGateway(object):
         self.wbk.save(nazwa_docelowa)
         self.wbk = None
 
+    def zapisz_direct(self, akt_wiersz, akt_kolumna, liczba, style=None):
+        '''
+        WriterGateway:
+        '''
+        dc_style = calculate_style(style)
+        self.sheet.write(akt_wiersz, akt_kolumna, liczba, **dc_style)
+
     def zapisz_mi(self, akt_wiersz, akt_kolumna, napis):
         '''
         WriterGateway:
@@ -254,13 +261,6 @@ class WriterGateway(object):
         WriterGateway:
         '''
         self.sheet.write(akt_wiersz, akt_kolumna, en_kw.utf_to_unicode(napis), self.n3_style)
-
-    def zapisz_direct(self, akt_wiersz, akt_kolumna, liczba, style=None):
-        '''
-        WriterGateway:
-        '''
-        dc_style = calculate_style(style)
-        self.sheet.write(akt_wiersz, akt_kolumna, liczba, **dc_style)
 
     def zapisz_ze_stylem(self, akt_wiersz, akt_kolumna, liczba, style):
         '''
