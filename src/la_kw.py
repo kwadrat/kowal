39f011ec2199098c8c669e7328c03e0168d1e1f9
Piotr Kasprzyk <ciri@ciri.pl>
2013-10-07 17:04:56 +0200

    Use calculate_style

--- src/la_kw.py
+++ src/la_kw.py
@@ -267,11 +267,12 @@ class WriterGateway(object):
         '''
         self.sheet.write(akt_wiersz, akt_kolumna, en_kw.utf_to_unicode(napis), self.n3_style)
 
-    def zapisz_ze_stylem(self, akt_wiersz, akt_kolumna, liczba, style):
+    def zapisz_ze_stylem(self, akt_wiersz, akt_kolumna, liczba, style=None):
         '''
         WriterGateway:
         '''
-        self.sheet.write(akt_wiersz, akt_kolumna, liczba, style)
+        dc_style = calculate_style(style)
+        self.sheet.write(akt_wiersz, akt_kolumna, liczba, **dc_style)
 
     def zapisz_flt(self, akt_wiersz, akt_kolumna, liczba, kl_miejsc=2):
         '''
