cc782c97a2c01c411078476fee64f9facdf48d15
Piotr Kasprzyk <ciri@ciri.pl>
2013-10-07 12:45:13 +0200

    Add n14_style

--- src/la_kw.py
+++ src/la_kw.py
@@ -142,6 +142,12 @@ class WriterGateway(object):
             horz=self.xlwt.Alignment.HORZ_CENTER,
             bold=1,
             )
+        self.n14_style = self.prepare_cell(
+            12,
+            vert=self.xlwt.Alignment.VERT_CENTER,
+            horz=self.xlwt.Alignment.HORZ_CENTER,
+            colour=self.xlwt.Style.colour_map['red'],
+            )
         self.decimal_digits = {
             0: self.n9_style,
             2: self.n1_style,
