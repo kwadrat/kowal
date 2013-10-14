9a7dec63ccc1980c325717f741e121fd05909ae4
Piotr Kasprzyk <ciri@ciri.pl>
2013-10-14 14:58:49 +0200

    Add TymczasowoWygenerujSztywne

--- src/rq_kw.py
+++ src/rq_kw.py
@@ -183,5 +183,6 @@ Niebezpieczne_testowa_aplikacja_produkcyjna_baza = 0
 
 DocelowoOdwrotnieOproczOstatniego = 1
 TymczasowoPozwolNaUzupelnienieBorki = 1
+TymczasowoWygenerujSztywne = 0
 
-TymczasowoTylkoJeden = 1
+TymczasowoTylkoJeden = 0
