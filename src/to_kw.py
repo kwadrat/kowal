1a8e12fddd493533231a6955dcfddcc9318b198a
Piotr Kasprzyk <ciri@ciri.pl>
2013-10-21 08:12:10 +0200

    Add to_kw.py

---
+++ src/to_kw.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+Single cell or multi-row/-column rectangle area
+'''
+
+import unittest
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+class MergedCoords(object):
+    def __init__(self):
+        '''
+        MergedCoords:
+        '''
+
+class TestMergedCoords(unittest.TestCase):
+    def test_merged_coords(self):
+        '''
+        TestMergedCoords:
+        '''
+        obk = MergedCoords()
+
