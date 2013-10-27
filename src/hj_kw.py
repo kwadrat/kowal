fb1f02fd4ecac94de744d9d2bf5d50badcc3b5dc
Piotr Kasprzyk <ciri@ciri.pl>
2013-10-27 17:45:33 +0100

    Add rcp_emax

--- src/hj_kw.py
+++ src/hj_kw.py
@@ -131,6 +131,9 @@ def rcp_poziom(etk_a, etk_b):
 def rcp_sred(etk_a, etk_b):
     return rcp_wspolne('AVERAGE', etk_a, etk_b)
 
+def rcp_emax(etk_a, etk_b):
+    return rcp_wspolne('MAX', etk_a, etk_b)
+
 def rcp_maxk(etk_a, etk_b, the_order):
     trzeci = ',%d' % (the_order + 1)
     return rcp_wspolne('LARGE', etk_a, etk_b, trzeci)
@@ -201,6 +204,7 @@ class TestProcessingSQL(unittest.TestCase):
         self.assertEqual(rcp_pion(0, 'B'), 'SUM(B2:B13)')
         self.assertEqual(rcp_poziom('E23', 'G23'), 'SUM(E23:G23)')
         self.assertEqual(rcp_sred('E23', 'G23'), 'AVERAGE(E23:G23)')
+        self.assertEqual(rcp_emax('E23', 'G23'), 'MAX(E23:G23)')
         self.assertEqual(rcp_maxk('C2', 'CT31', 0), 'LARGE(C2:CT31,1)')
         self.assertEqual(rcp_maxk('C2', 'CT31', 1), 'LARGE(C2:CT31,2)')
         a = [1, 4, 2, 3]; reverse_but_last(a)
