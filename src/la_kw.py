629a7c0238cb4ce88b44cccbdb7deaf026403bdf
Piotr Kasprzyk <ciri@ciri.pl>
2013-10-28 13:43:19 +0100

    Add bold

--- src/la_kw.py
+++ src/la_kw.py
@@ -320,12 +320,12 @@ class WriterGateway(object):
         the_style = self.get_or_generate_style(rn_colour, bold, size, wrap, middle, kl_none)
         self.zapisz_mi(m_coor, tresc_napisu, style=the_style)
 
-    def napis_ze_wsp(self, row, col, tresc_napisu=None, rn_colour=None):
+    def napis_ze_wsp(self, row, col, tresc_napisu=None, bold=None, rn_colour=None):
         '''
         WriterGateway:
         '''
         m_coor = to_kw.MergedCoords(row, col)
-        self.napis_ze_stylem(m_coor, tresc_napisu, rn_colour=rn_colour)
+        self.napis_ze_stylem(m_coor, tresc_napisu, bold=bold, rn_colour=rn_colour)
 
     def wymus_szerokosci(self, lista_rozmiarow):
         '''
