2ea3d61f8cfafd7164687bb7b0e1172efd763bc4
Piotr Kasprzyk <ciri@ciri.pl>
2013-10-28 14:13:05 +0100

    Add italic

--- src/la_kw.py
+++ src/la_kw.py
@@ -313,7 +313,7 @@ class WriterGateway(object):
         m_coor = to_kw.MergedCoords(akt_wiersz, akt_kolumna)
         self.write_single(m_coor, tresc_napisu, style=the_style)
 
-    def napis_ze_stylem(self, m_coor, tresc_napisu=None, rn_colour=None, bold=0, size=None, wrap=None, middle=None, kl_none=None):
+    def napis_ze_stylem(self, m_coor, tresc_napisu=None, rn_colour=None, bold=0, size=None, wrap=None, middle=None, kl_none=None, italic=None):
         '''
         WriterGateway:
         '''
@@ -325,7 +325,7 @@ class WriterGateway(object):
         WriterGateway:
         '''
         m_coor = to_kw.MergedCoords(row, col)
-        self.napis_ze_stylem(m_coor, tresc_napisu, bold=bold, rn_colour=rn_colour)
+        self.napis_ze_stylem(m_coor, tresc_napisu, bold=bold, rn_colour=rn_colour, italic=italic)
 
     def wymus_szerokosci(self, lista_rozmiarow):
         '''
