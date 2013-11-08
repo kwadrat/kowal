3435af66d31be2b2d25650f94c2f69fd1f9bf5bf
Piotr Kasprzyk <ciri@ciri.pl>
2013-11-08 06:05:44 +0100

    Add HourEstimate

--- src/ja_kw.py
+++ src/ja_kw.py
@@ -16,6 +16,37 @@ for i in NazwyModulow:
         else:
             exec 'import %(modul)s' % dict(modul = i)
 
+class HourEstimate(object):
+    def store_value(self, quarter_index, one_sample):
+        '''
+        HourEstimate:
+        '''
+        self.quarter_index = quarter_index
+        self.one_sample = one_sample
+
+    def __init__(self, full_hour):
+        '''
+        HourEstimate:
+        '''
+        self.full_hour = full_hour
+        self.store_value(None, None)
+
+    def consult_value(self, quarter_index, one_sample):
+        '''
+        HourEstimate:
+        '''
+        if one_sample is not None:
+            if self.one_sample is None:
+                self.store_value(quarter_index, one_sample)
+            elif one_sample > self.one_sample:
+                self.store_value(quarter_index, one_sample)
+
+    def has_interesting_data(self):
+        '''
+        HourEstimate:
+        '''
+        return self.quarter_index is not None
+
 class MonthSummary(object):
     def __init__(self):
         '''
@@ -34,6 +65,20 @@ class MonthSummary(object):
                 pass
 
 class TestMonthStatistics(unittest.TestCase):
+    def test_hour_estimate(self):
+        '''
+        TestMonthStatistics:
+        '''
+        obk = HourEstimate(full_hour=23)
+        quarter_index = 0
+        one_sample = None
+        obk.consult_value(quarter_index, one_sample)
+        self.assertEqual(obk.has_interesting_data(), 0)
+        quarter_index = 1
+        one_sample = 5
+        obk.consult_value(quarter_index, one_sample)
+        self.assertEqual(obk.has_interesting_data(), 1)
+
     def test_month_statistics(self):
         '''
         TestMonthStatistics:
