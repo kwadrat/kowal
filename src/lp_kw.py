551cc15cc11eec1967b8ea2b0a8edc5c6892a656
Piotr Kasprzyk <ciri@ciri.pl>
2013-11-08 08:59:50 +0100

    Add dst_double_hour

--- src/lp_kw.py
+++ src/lp_kw.py
@@ -74,6 +74,19 @@ class HoQuServer(object):
         self.smpl_per_hour = smpl_per_hour
         self.cnt_of_samples = len(self.time_for_header())
 
+    def dst_double_hour(self, row_date, sample_index):
+        '''
+        HoQuServer:
+        '''
+        result = 0
+        if row_date == '2013-10-27' or row_date == '2012-10-28':
+            if self.smpl_per_hour == 4:
+                result = 7 <= sample_index <= 10
+            else:
+                assert self.smpl_per_hour == 1
+                result = sample_index == 1
+        return result
+
 class HourServer(HoQuServer):
     def __init__(self):
         '''
