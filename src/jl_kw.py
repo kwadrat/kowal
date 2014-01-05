d10be573a3650f99ea792974b0587249b98f0786
Piotr Kasprzyk <ciri@ciri.pl>
2014-01-05 12:16:14 +0100

    verify_hours_headers - remove original

--- src/jl_kw.py
+++ src/jl_kw.py
@@ -33,15 +33,6 @@ class HourServer(HoQuServer):
             self.time_for_header.append(elem.header_for_hour_column)
         HoQuServer.__init__(self, 1)
 
-    def verify_hours_headers(self, energy_reader, start_energy_col):
-        '''
-        HourServer:
-        '''
-        for sample_index, one_column in enumerate(self.all_time_columns):
-            tmp_text = energy_reader.vx_num_time(start_energy_col + sample_index, 6)
-            expected = one_column.header_for_hour_column
-            lp_kw.verify_for_equal(tmp_text, expected)
-
 class TestHour_t_Server(unittest.TestCase):
     def test_server_of_hours(self):
         '''
