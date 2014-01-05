0cfd0fcaec471dba092b45fd74e84e4770d9c45a
Piotr Kasprzyk <ciri@ciri.pl>
2014-01-05 21:12:45 +0100

    Use autumn_dst_day

--- src/jn_kw.py
+++ src/jn_kw.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+import dn_kw
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
 class HoQuServer(object):
     def __init__(self, smpl_per_hour):
         '''
@@ -14,7 +27,7 @@ class HoQuServer(object):
         HoQuServer:
         '''
         result = 0
-        if row_date == '2013-10-27' or row_date == '2012-10-28':
+        if dn_kw.autumn_dst_day(row_date):
             if self.smpl_per_hour == 4:
                 result = 7 <= sample_index <= 10
             else:
