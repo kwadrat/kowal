cca9127c02882ca2a6e8667070e373dd002dd595
Piotr Kasprzyk <ciri@ciri.pl>
2014-01-31 19:14:30 +0100

    Add FenceMaker

---
+++ src/nf_kw.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import unittest
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+class FenceMaker(object):
+    def __init__(self):
+        '''
+        FenceMaker:
+        '''
+
+class TestFenceShape(unittest.TestCase):
+    def test_fence_shape(self):
+        '''
+        TestFenceShape:
+        '''
+        obk = FenceMaker()
