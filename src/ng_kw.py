27787a455ec99163747f7a66752208cb8ed180a3
Piotr Kasprzyk <ciri@ciri.pl>
2014-02-05 22:11:07 +0100

    Add NVB_3_STYLE

---
+++ src/ng_kw.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+'''
+Etykiety dla styli generowanych plików Excel
+'''
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+code_strings = '''
+NVB_3_STYLE
+'''
+for single_name in code_strings.split():
+    exec '%(single_name)s = "%(single_name)s"' % dict(
+        single_name=single_name,
+        )
