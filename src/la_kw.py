b8e38b656c01919982c7d980f98861129cf2fdc9
Piotr Kasprzyk <ciri@ciri.pl>
2014-02-19 09:13:26 +0100

    Add liczba_wierszy

--- src/la_kw.py
+++ src/la_kw.py
@@ -329,11 +329,11 @@ class WriterGateway(object):
         style = self.convert_from_sel(style_sel)
         self.write_utf(m_coor, napis, style)
 
-    def zapisz_l_polaczone_komorki(self, akt_wiersz, akt_kolumna, napis, style_sel, liczba_kolumn=1):
+    def zapisz_l_polaczone_komorki(self, akt_wiersz, akt_kolumna, napis, style_sel, liczba_kolumn=1, liczba_wierszy=1):
         '''
         WriterGateway:
         '''
-        m_coor = to_kw.MergedCoords(akt_wiersz, akt_kolumna, liczba_kolumn)
+        m_coor = to_kw.MergedCoords(akt_wiersz, akt_kolumna, liczba_kolumn, liczba_wierszy)
         self.write_l_utf(m_coor, napis, style_sel)
 
     def zapisz_rozmiar_14_komorki(self, akt_wiersz, akt_kolumna, napis, liczba_kolumn=8):
