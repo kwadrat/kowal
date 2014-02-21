b961a282c8f56c04c8991ec9f4c9fa0ca2d0c967
Piotr Kasprzyk <ciri@ciri.pl>
2014-02-21 09:24:45 +0100

    Reduce n17_style

--- src/la_kw.py
+++ src/la_kw.py
@@ -187,13 +187,6 @@ class WriterGateway(object):
             NMF_3_date: 'yyyy/mm/dd;@', # data RRRR-MM-DD
             }
         self.xlwt = new_module_for_writing_spreadsheet()
-        self.n17_style = self.prepare_cell(
-            12,
-            vert=self.xlwt.Alignment.VERT_CENTER,
-            horz=self.xlwt.Alignment.HORZ_CENTER,
-            borders=nf_kw.brd_1_obk,
-            wrap=1,
-            )
         self.n19_style = self.prepare_cell(
             12,
             bold=1,
@@ -257,7 +250,13 @@ class WriterGateway(object):
                 horz=self.xlwt.Alignment.HORZ_CENTER,
                 borders=nf_kw.brd_1_obk,
                 ),
-            ng_kw.NVB_17_STYLE: self.n17_style,
+            ng_kw.NVB_17_STYLE: self.prepare_cell(
+                12,
+                vert=self.xlwt.Alignment.VERT_CENTER,
+                horz=self.xlwt.Alignment.HORZ_CENTER,
+                borders=nf_kw.brd_1_obk,
+                wrap=1,
+                ),
             ng_kw.NVB_19_STYLE: self.n19_style,
             }
 
