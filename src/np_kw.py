83ab31e3f990f894c237c58297333bfeb8933ed3
Piotr Kasprzyk <ciri@ciri.pl>
2014-02-21 09:51:33 +0100

    Reorder

--- src/np_kw.py
+++ src/np_kw.py
@@ -49,8 +49,8 @@ class PowerWriter(CommonWriter):
         ten_treshold = samples_in_order[-10]
         for sample_index, my_sample in enumerate(my_data[lc_kw.fq_m_samples_qv]):
             col = self.first_sample_column + sample_index
-            m_coor = to_kw.MergedCoords(row, col)
             dc_b_style = jc_kw.obtain_cell_color(moc_um_dec, ten_treshold, jestem_weekend, my_sample)
+            m_coor = to_kw.MergedCoords(row, col)
             xwg.zapisz_co_flt(m_coor, my_sample, **dc_b_style)
 
     def generate_for_month(self, xwg, dane_bazy, nr_month, dost_wiersz, moc_umowna):
