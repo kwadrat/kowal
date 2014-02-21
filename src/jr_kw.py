5cdd0aef8716596b43cb7a00f281646cd2f27349
Piotr Kasprzyk <ciri@ciri.pl>
2014-02-21 17:44:52 +0100

    Add jr_kw

---
+++ src/jr_kw.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+Koszty jednostkowe energii elektrycznej dla C-21, odczytywane
+z arkusza Excel, dla roku 2010, gdzie zmiany taryfy były
+z dokładnością do dnia (a nie do miesiąca)
+'''
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+import jv_kw
+import gu_kw
+import jt_kw
+import la_kw
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+ManipulateSheet = jv_kw.ManipulateSheet
+
+class EnergyMonthUnitCosts(ManipulateSheet):
+    def __init__(self, wbk):
+        '''
+        EnergyMonthUnitCosts:
+        '''
+        ManipulateSheet.__init__(self)
+        self.wbk = wbk
+        klm_a_ads = gu_kw.KolumnowyAdresator()
+        for self.sheet in self.wbk.sheets()[:17]:
+            if 0:
+                print self.sheet.name
+            klm_a_ads.set_ka_base_address('E8')
+            pracuj = 1
+            d_col = 0
+            while 1:
+                wiersz, kolumna = klm_a_ads.get_row_col(col_delta=d_col)
+                wartosc_parametru = self.read_cell(wiersz, kolumna)
+                if jt_kw.valid_date_format(wartosc_parametru):
+                    print wartosc_parametru
+                    d_col += 1
+                else:
+                    break
+
+def generate_unit_2_cost_data(dfb, filename):
+    xlrd = la_kw.new_module_for_reading_spreadsheet()
+    wbk = xlrd.open_workbook(filename)
+    energy_unit_costs = EnergyMonthUnitCosts(wbk)
