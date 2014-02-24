e94493ec55df4e08fd3c2524375067c2e4095a0d
Piotr Kasprzyk <ciri@ciri.pl>
2014-02-24 10:39:48 +0100

    Remove get_or_generate_number_style

--- src/la_kw.py
+++ src/la_kw.py
@@ -142,42 +142,6 @@ class WriterGateway(object):
                 self.generated_string_style_cache[the_key] = the_style
         return the_style
 
-    def get_or_generate_number_style(self, kl_miejsc=None, rn_colour=None, bold=None, size=None, wrap=None, middle=None, kl_none=None, italic=None, borders=None, fore_colour=None):
-        '''
-        WriterGateway:
-        '''
-        if kl_none:
-            the_style = None
-        else:
-            the_key = (kl_miejsc, rn_colour, bold, size, wrap, middle, italic, borders, fore_colour)
-            the_style = self.generated_string_style_cache.get(the_key)
-            if the_style is None:
-                dc_params = {}
-                if rn_colour is not None:
-                    colour = self.xlwt.Style.colour_map[rn_colour]
-                    dc_params['colour'] = colour
-                if bold is not None:
-                    dc_params['bold'] = bold
-                if size is not None:
-                    dc_params['size'] = size
-                if wrap is not None:
-                    dc_params['wrap'] = wrap
-                if borders is not None:
-                    dc_params['borders'] = borders
-                if italic is not None:
-                    dc_params['italic'] = italic
-                if middle:
-                    dc_params['vert'] = self.xlwt.Alignment.VERT_CENTER
-                    dc_params['horz'] = self.xlwt.Alignment.HORZ_CENTER
-                if fore_colour is not None:
-                    colour = self.xlwt.Style.colour_map[fore_colour]
-                    dc_params['fore_colour'] = colour
-                if kl_miejsc is not None:
-                    dc_params['num_format_str'] = self.format_map[kl_miejsc]
-                the_style = self.prepare_cell(**dc_params)
-                self.generated_string_style_cache[the_key] = the_style
-        return the_style
-
     def __init__(self):
         '''
         WriterGateway:
