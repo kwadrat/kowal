684381b0175358487c60aecae2bb8c4dcc841370
Piotr Kasprzyk <ciri@ciri.pl>
2014-02-24 09:41:44 +0100

    Use non_wrapped_lines

--- src/zr_kw.py
+++ src/zr_kw.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+import jr_kw
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
 wykaz_pytan = '''\
 '''
 
@@ -10,4 +23,4 @@ def wykonaj_zapytanie(dfb):
     wynik = dfb.query_old_dct(pytanie_reczne)
     for rekord in wynik:
         print rekord
-    print '# ' 'v' 'i' 'm' ': nowrap'
+    print jr_kw.non_wrapped_lines()
