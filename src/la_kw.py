c94af577eb9e190b8a1899df6c9a75caca54ef97
Piotr Kasprzyk <ciri@ciri.pl>
2014-02-24 16:19:26 +0100

    Change default for middle

--- src/la_kw.py
+++ src/la_kw.py
@@ -432,7 +432,7 @@ class WriterGateway(object):
         m_coor = to_kw.MergedCoords(akt_wiersz, akt_kolumna)
         self.write_unicode(m_coor, the_content, the_style)
 
-    def ustaw_sam_styl(self, akt_wiersz, akt_kolumna, the_content=None, kl_miejsc=None, rn_colour=None, bold=0, size=None, middle=1, italic=None, fore_colour=None):
+    def ustaw_sam_styl(self, akt_wiersz, akt_kolumna, the_content=None, kl_miejsc=None, rn_colour=None, bold=0, size=None, middle=None, italic=None, fore_colour=None):
         '''
         WriterGateway:
         '''
