1c4dea5c10450be15e3b60cc54997180e7d79ee6
Piotr Kasprzyk <ciri@ciri.pl>
2014-02-24 14:50:49 +0100

    Move get_or_generate_style

--- src/la_kw.py
+++ src/la_kw.py
@@ -391,8 +391,8 @@ class WriterGateway(object):
         '''
         WriterGateway:
         '''
-        m_coor = to_kw.MergedCoords(akt_wiersz, akt_kolumna)
         the_style = self.get_or_generate_style(kl_miejsc=NMF_3_date, middle=1, italic=italic, fore_colour=fore_colour)
+        m_coor = to_kw.MergedCoords(akt_wiersz, akt_kolumna)
         self.write_unicode(m_coor, the_content, the_style)
 
     def zapisz_wzor(self, m_coor, tekst_wzoru, kl_miejsc=2, size=None, bold=None, rn_colour=None, borders=None):
