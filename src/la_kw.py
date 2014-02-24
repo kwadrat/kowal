57663c677f4afb9d13b011b82396fb0db4d1bc48
Piotr Kasprzyk <ciri@ciri.pl>
2014-02-24 12:20:16 +0100

    Use ustaw_sam_styl

--- src/la_kw.py
+++ src/la_kw.py
@@ -439,8 +439,7 @@ class WriterGateway(object):
         WriterGateway:
         '''
         the_content = en_kw.utf_to_unicode(tresc_napisu)
-        m_coor = to_kw.MergedCoords(row, col)
-        self.napis_ze_stylem(m_coor, the_content, rn_colour=rn_colour, bold=bold, middle=middle, italic=italic, fore_colour=fore_colour)
+        self.ustaw_sam_styl(row, col, the_content=the_content, kl_miejsc=None, rn_colour=rn_colour, bold=bold, middle=middle, italic=italic, fore_colour=fore_colour)
 
     def wymus_szerokosci(self, lista_rozmiarow):
         '''
