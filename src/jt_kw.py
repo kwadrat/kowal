825d9871bc7c6b11099f9f18740beedd917a8f64
Piotr Kasprzyk <ciri@ciri.pl>
2014-03-05 19:18:45 +0100

    Add spaces

--- src/jt_kw.py
+++ src/jt_kw.py
@@ -123,10 +123,10 @@ class RomanPeriod(object):
         '''
         RomanPeriod:
         '''
-        the_first=nazwa_rzymskiego(self.the_first)
+        the_first = nazwa_rzymskiego(self.the_first)
         if self.the_day_first is not None:
             the_first = '%02d.%s' % (self.the_day_first, the_first)
-        the_second=nazwa_rzymskiego(self.the_second)
+        the_second = nazwa_rzymskiego(self.the_second)
         return '%(the_first)s-%(the_second)s-%(the_year)04d' % dict(
             the_first=the_first,
             the_second=the_second,
