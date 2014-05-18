a37bdfdedffebb788b56599f74970297ec24faf9
Piotr Kasprzyk <ciri@ciri.pl>
2014-05-18 13:50:08 +0200

    Add underline

--- src/la_kw.py
+++ src/la_kw.py
@@ -355,11 +355,11 @@ class WriterGateway(object):
         '''
         self.zapisz_l_polaczone_komorki(akt_wiersz, akt_kolumna, napis, style_sel=ng_kw.NVB_17_STYLE)
 
-    def zapisz_flt(self, akt_wiersz, akt_kolumna, the_content, kl_miejsc=None, rn_colour=None, bold=None, size=None, middle=1, italic=None, borders=None, fore_colour=None, liczba_kolumn=1, liczba_wierszy=1):
+    def zapisz_flt(self, akt_wiersz, akt_kolumna, the_content, kl_miejsc=None, rn_colour=None, bold=None, size=None, middle=1, italic=None, underline=None, borders=None, fore_colour=None, liczba_kolumn=1, liczba_wierszy=1):
         '''
         WriterGateway:
         '''
-        the_style = self.get_or_generate_style(kl_miejsc=kl_miejsc, rn_colour=rn_colour, bold=bold, size=size, middle=middle, italic=italic, borders=borders, fore_colour=fore_colour)
+        the_style = self.get_or_generate_style(kl_miejsc=kl_miejsc, rn_colour=rn_colour, bold=bold, size=size, middle=middle, italic=italic, underline=underline, borders=borders, fore_colour=fore_colour)
         self.zapisz_s_polaczone_komorki(akt_wiersz, akt_kolumna, the_content, the_style, liczba_kolumn=liczba_kolumn, liczba_wierszy=liczba_wierszy)
 
     def zapisz_rn_flt(self, akt_wiersz, akt_kolumna, rn_liczba):
