ae4f28e0dd69057736e17fe5a9fd38138685da41
Piotr Kasprzyk <ciri@ciri.pl>
2014-05-21 06:34:31 +0200

    Add LpFakturyRoku

---
+++ src/ud_kw.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import unittest
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+class LpFakturyRoku(object):
+    def __init__(self):
+        '''
+        LpFakturyRoku:
+        '''
+
+class TestLpFaktury(unittest.TestCase):
+    def test_inserting_all(self):
+        '''
+        TestLpFaktury:
+        '''
+        obk = LpFakturyRoku()
