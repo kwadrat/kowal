61f621ae2a9178d1816ce0abff3909831a7de484
Piotr Kasprzyk <ciri@ciri.pl>
2014-06-20 10:56:27 +0200

    Remove DocelowoZmieniajPochodneFaktury

--- src/ust_iw_kw.py
+++ src/ust_iw_kw.py
@@ -12,8 +12,6 @@ LokalnaDiagnostykaKlas = 0 and TymczasowoSzczegolowaWizualizacja
 # Wizualizacja działania nadliczników
 TymczasowoWizualizacjaNadlicznikow = 0 and TymczasowoSzczegolowaWizualizacja
 
-DocelowoZmieniajPochodneFaktury = 1
-
 # Wizualizacja działania słowników zapalających licznikowane,
 # czyli dopasowania wody i ścieków
 TymczasowoWizualizacjaLicznikowanych = 0 and TymczasowoSzczegolowaWizualizacja
