4f5e52b969fb39d810f767a28bb2ae909992be29
Piotr Kasprzyk <ciri@ciri.pl>
2014-06-21 20:31:30 +0200

    Check JestemObca

--- src/jo_kw.py
+++ src/jo_kw.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+import unittest
+
 class JestemObca(object):
     '''Informuje, że faktura jest obca lub przykryta fakturą korygującą'''
 
@@ -42,3 +44,41 @@ class JestemObca(object):
         return ' '.join(wynik)
 
 ob_fjo = JestemObca()
+
+class TestObcejFaktury(unittest.TestCase):
+
+    def test_0_obcej_faktury(self):
+        '''
+        TestObcejFaktury:
+        '''
+        obk = JestemObca()
+        obk.okresl_obcosc(obcosc=0, przykrycie=0)
+        self.assertEqual(obk.jestem_specjalna(), 0)
+        self.assertEqual(obk.jestem_przykryta(), 0)
+
+    def test_1_obcej_faktury(self):
+        '''
+        TestObcejFaktury:
+        '''
+        obk = JestemObca()
+        obk.okresl_obcosc(obcosc=0, przykrycie=1)
+        self.assertEqual(obk.jestem_specjalna(), 1)
+        self.assertEqual(obk.jestem_przykryta(), 1)
+
+    def test_2_obcej_faktury(self):
+        '''
+        TestObcejFaktury:
+        '''
+        obk = JestemObca()
+        obk.okresl_obcosc(obcosc=1, przykrycie=0)
+        self.assertEqual(obk.jestem_specjalna(), 1)
+        self.assertEqual(obk.jestem_przykryta(), 0)
+
+    def test_3_obcej_faktury(self):
+        '''
+        TestObcejFaktury:
+        '''
+        obk = JestemObca()
+        obk.okresl_obcosc(obcosc=1, przykrycie=1)
+        self.assertEqual(obk.jestem_specjalna(), 1)
+        self.assertEqual(obk.jestem_przykryta(), 1)
