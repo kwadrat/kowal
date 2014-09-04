c5831121d54b05671656c5c3f2ab54c22cb914ba
Piotr Kasprzyk <ciri@ciri.pl>
2014-09-04 05:31:50 +0200

    Add TxtSheet in um_kw.py

---
+++ src/um_kw.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import unittest
+
+NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+import lc_kw
+import fv_kw
+import le_kw
+import lp_kw
+import lq_kw
+import eo_kw
+import tq_kw
+'''.splitlines()]
+
+for i in NazwyModulow:
+    if i == __name__.split('.')[-1]:
+        raise RuntimeError('Modul laduje sam siebie?: %s' % repr(i))
+    else:
+        if i in globals():
+            exec '%(modul)s = reload(%(modul)s)' % dict(modul = i)
+        else:
+            exec 'import %(modul)s' % dict(modul = i)
+
+def normalize_value(before):
+    if before == '':
+        result = None
+    else:
+        result = before
+    return result
+
+CommonRdWr = tq_kw.CommonRdWr
+
+class TxtSheet(object):
+    def __init__(self):
+        '''
+        TxtSheet:
+        '''
+
+class TestSheetInText(unittest.TestCase):
+    def test_sheet_in_text(self):
+        '''
+        TestSheetInText:
+        '''
+        obk = TxtSheet()
