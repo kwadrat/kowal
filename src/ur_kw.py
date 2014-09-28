be90df46ea50af956c3fbef6a753414ddeada2d1
Piotr Kasprzyk <ciri@ciri.pl>
2014-09-28 13:36:54 +0200

    Add xldate_as_tuple, open_workbook

--- src/ur_kw.py
+++ src/ur_kw.py
@@ -4,6 +4,8 @@
 import unittest
 
 NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
+import ut_kw
+import dz_kw
 '''.splitlines()]
 
 for i in NazwyModulow:
@@ -21,9 +23,31 @@ class TxtXlrd(object):
         TxtXlrd:
         '''
 
+    def xldate_as_tuple(self, value, datemode):
+        '''
+        TxtXlrd:
+        '''
+        result = None
+        hour_minute = dz_kw.extract_csv_hour(value)
+        if hour_minute is not None:
+            result = tuple([0, 0, 0] + hour_minute + [0])
+        return result
+
+    def open_workbook(self, single_file):
+        '''
+        TxtXlrd:
+        '''
+        return ut_kw.TxtBook(single_file)
+
 class TestXlrdInText(unittest.TestCase):
     def test_xlrd_in_text(self):
         '''
         TestXlrdInText:
         '''
         obk = TxtXlrd()
+        value = '1:00'
+        odp = obk.xldate_as_tuple(value, None)
+        self.assertEqual(odp, (0, 0, 0, 1, 0, 0))
+        value = '2:00'
+        odp = obk.xldate_as_tuple(value, None)
+        self.assertEqual(odp, (0, 0, 0, 2, 0, 0))
