88b720f90101a25ecf4814a2b82528871d148ddc
Piotr Kasprzyk <ciri@ciri.pl>
2014-11-20 09:00:52 +0100

    Add official_name

--- src/ib_kw.py
+++ src/ib_kw.py
@@ -25,6 +25,10 @@ class FieldDesc(object):
         '''
         self.moje_pole = moje_pole
         self.fs_prefix = fs_prefix
+        if self.fs_prefix is None:
+            self.official_name = self.moje_pole
+        else:
+            self.official_name = self.fs_prefix + self.moje_pole
 
 class TestMojegoPola(unittest.TestCase):
     def test_mojego_pola(self):
@@ -34,3 +38,13 @@ class TestMojegoPola(unittest.TestCase):
         obk = FieldDesc('pole', 'przed_')
         self.assertEqual(obk.moje_pole, 'pole')
         self.assertEqual(obk.fs_prefix, 'przed_')
+        self.assertEqual(obk.official_name, 'przed_pole')
+
+    def test_bez_przedrostka(self):
+        '''
+        TestMojegoPola:
+        '''
+        obk = FieldDesc('pole', None)
+        self.assertEqual(obk.moje_pole, 'pole')
+        self.assertEqual(obk.fs_prefix, None)
+        self.assertEqual(obk.official_name, 'pole')
