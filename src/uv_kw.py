88c5fa4b5a6a743dfcc0b25725b946a46c8767f2
Piotr Kasprzyk <ciri@ciri.pl>
2014-12-31 13:50:14 +0100

    Add counter_person_matches

--- src/uv_kw.py
+++ src/uv_kw.py
@@ -35,6 +35,12 @@ class MojPodnajemca(object):
                 result = 1
         return result
 
+    def counter_person_matches(self, numer_nadlicznika, dane_osoby):
+        '''
+        MojPodnajemca:
+        '''
+        return 0
+
 class TestJednegoPodnajemcy(unittest.TestCase):
     def test_jednego_podnajemcy(self):
         '''
@@ -64,3 +70,10 @@ class TestJednegoPodnajemcy(unittest.TestCase):
         obk = MojPodnajemca(None, 'KOWALSKI JAN', None, '2014-02-27')
         self.assertEqual(obk.date_in_range('2014-02-27'), 1)
         self.assertEqual(obk.date_in_range('2014-02-28'), 0)
+
+    def test_other_person_invoice(self):
+        '''
+        TestJednegoPodnajemcy:
+        '''
+        obk = MojPodnajemca('00003-001', 'NOWAK ADAM', None, None)
+        self.assertEqual(obk.counter_person_matches('00003-001', 'THE BUILDING'), 0)
