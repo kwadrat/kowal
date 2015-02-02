55b1142a3258676291cf7308cdbb14d1fcfd7167
Piotr Kasprzyk <ciri@ciri.pl>
2015-02-02 13:11:20 +0100

    Copy from rozbij_na_pola

--- src/ug_kw.py
+++ src/ug_kw.py
@@ -26,11 +26,56 @@ class FieldSplitter(object):
         FieldSplitter:
         '''
 
-    def split_fields(self, line):
+    def split_fields(self, line, quoting=0):
         '''
         FieldSplitter:
         '''
-        return line.split(delimiter)
+        t = []
+        inside_quote = 0
+        cur_ptr = 0 # Wskaźnik na aktualny analizowany znak linii
+        ln_len = len(line) # Długość linii
+        first_char = 1 # Znacznik - analizujemy pierwszy znak pola wiersza
+        work_flag = 1
+        while work_flag:
+            if first_char:
+                first_char = 0
+                if quoting and cur_ptr < ln_len and line[cur_ptr] == quotechar:
+                    inside_quote = 1
+                    cur_ptr += 1 # Pomiń cudzysłów
+                else:
+                    inside_quote = 0
+                strt_pos = cur_ptr # To jest pierwszy znak pola do zapamiętania
+            if inside_quote:
+                # Szukamy zamykającego cudzysłowu
+                if cur_ptr >= ln_len:
+                    raise RuntimeError, "Wychodzimy poza linię o treści:\n" + line
+                elif quoting and line[cur_ptr] == quotechar:
+                    if cur_ptr + 1 < ln_len and line[cur_ptr + 1] == quotechar: # Pominiemy, jeśli cudzysłów jest podwójny
+                        cur_ptr += 2
+                    else:
+                        # Pojedynczy cudzysłów - koniec napisu
+                        t.append(line[strt_pos:cur_ptr].replace(PodwZnkSepTekstu, quotechar))
+                        cur_ptr += 1
+                        # Tu czekamy na przecinek
+                        if cur_ptr < ln_len and line[cur_ptr] == delimiter:
+                            cur_ptr += 1
+                            first_char = 1 # Znowu zaczynamy analizę od pierwszego znaku pola
+                        else:
+                            # Nie było przecinka - koniec analizy
+                            work_flag = 0
+                else:
+                    cur_ptr += 1 # Szukamy następnego znaku
+            else: # Nie mamy cudzysłowu - szukamy do następnego przecinka lub końca
+                if cur_ptr >= ln_len or line[cur_ptr] in znaki_konczace_pole_tekstowe:
+                    t.append(line[strt_pos:cur_ptr])
+                    if cur_ptr < ln_len and line[cur_ptr] == delimiter:
+                        cur_ptr += 1 # Będzie kolejne pole
+                        first_char = 1
+                    else:
+                        work_flag = 0 # Koniec analizy - był Enter
+                else: # Zwykły znak - przesuwamy się dalej
+                    cur_ptr += 1
+        return t
 
 def rozbij_na_pola(line, quoting=0):
     t = []
