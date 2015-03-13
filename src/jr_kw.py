e69e414ef05c4b99753db638581c396eb9b2d68f
Piotr Kasprzyk <ciri@ciri.pl>
2015-03-13 02:06:29 +0100

    Remove EnergyMonthUnitCosts

--- src/jr_kw.py
+++ src/jr_kw.py
@@ -10,9 +10,6 @@ z dokładnością do dnia (a nie do miesiąca)
 import unittest
 
 NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
-import jv_kw
-import gu_kw
-import jt_kw
 import la_kw
 '''.splitlines()]
 
@@ -76,41 +73,6 @@ class EnergyTable(object):
         self.ls_lines = map(lambda x: common_frmt % x, self.ls_lines)
         self.ls_lines.append(non_wrapped_lines())
         print '\n'.join(self.ls_lines)
-
-ManipulateSheet = jv_kw.ManipulateSheet
-
-class EnergyMonthUnitCosts(ManipulateSheet):
-    def col_details(self, en_tb, d_col, wiersz, kolumna):
-        '''
-        EnergyMonthUnitCosts:
-        '''
-        for i in [0, 2, 3, 4, 5, 7, 8]:
-            kawalek = '%14s' % self.read_cell(wiersz + i, kolumna)
-            en_tb.set_elem(i, d_col, kawalek)
-
-    def __init__(self, wbk):
-        '''
-        EnergyMonthUnitCosts:
-        '''
-        ManipulateSheet.__init__(self)
-        self.wbk = wbk
-        klm_a_ads = gu_kw.KolumnowyAdresator()
-        en_tb = EnergyTable()
-        for self.sheet in self.wbk.sheets()[:17]:
-            en_tb.set_elem(-1, 0, self.sheet.name)
-            klm_a_ads.set_ka_base_address('E8')
-            pracuj = 1
-            d_col = 0
-            while 1:
-                wiersz, kolumna = klm_a_ads.get_row_col(col_delta=d_col)
-                wartosc_parametru = self.read_cell(wiersz, kolumna)
-                if jt_kw.valid_date_format(wartosc_parametru):
-                    self.col_details(en_tb, d_col, wiersz, kolumna)
-                    d_col += 1
-                else:
-                    break
-            en_tb.end_object()
-        en_tb.show_summary()
 
 def workbook_for_verbose_reading(filename):
     xlrd = la_kw.new_module_for_reading_spreadsheet()
