f46d140880f049f5f3d9da31363fd51e5a3381c7
Piotr Kasprzyk <ciri@ciri.pl>
2015-07-14 21:07:07 +0200

    Reduce DocelowoObiektoweSezonyGrzewcze

--- src/rq_kw.py
+++ src/rq_kw.py
@@ -190,7 +190,6 @@ Niebezpieczne_testowa_aplikacja_produkcyjna_baza = 0
 
 DocelowoOdwrotnieOproczOstatniego = 1
 TymczasowoPozwolNaUzupelnienieBorki = 0
-DocelowoObiektoweSezonyGrzewcze = 1
 
 DocelowoPosrednikDokladnejLiczby = 0
 TymczasowoPokazWzory = 0
