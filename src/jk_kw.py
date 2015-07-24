01d8916e740a1951566dd3366a9b8e64cbcc7de3
Piotr Kasprzyk <ciri@ciri.pl>
2015-07-24 12:47:02 +0200

    Rebuild JednoKrotny

--- src/jk_kw.py
+++ src/jk_kw.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+import unittest
+
 NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
 '''.splitlines()]
 
@@ -14,11 +16,12 @@ for i in NazwyModulow:
             exec 'import %(modul)s' % dict(modul = i)
 
 class JednoKrotny(object):
-    def __init__(self):
+    def __init__(self, use_bool=0):
         '''
         JednoKrotny:
         '''
         self.licznik = 0
+        self.use_bool = use_bool
 
     def wywolaj_jednokrotnie(self):
         '''
@@ -26,4 +29,24 @@ class JednoKrotny(object):
         '''
         self.licznik += 1
         if self.licznik > 1:
-            raise RuntimeError('Zostałem wywołany kolejny raz')
+            if not self.use_bool:
+                raise RuntimeError('Zostałem wywołany kolejny raz')
+        return self.licznik == 1
+
+class TestCallAtMostOnce(unittest.TestCase):
+    def test_call_at_most_once(self):
+        '''
+        TestCallAtMostOnce:
+        '''
+        ojt = JednoKrotny()
+        ojt.wywolaj_jednokrotnie()
+        self.assertRaises(RuntimeError, ojt.wywolaj_jednokrotnie)
+
+    def test_a_call_at_most_once(self):
+        '''
+        TestCallAtMostOnce:
+        '''
+        ojt = JednoKrotny(use_bool=1)
+        self.assertEqual(ojt.wywolaj_jednokrotnie(), 1)
+        self.assertEqual(ojt.wywolaj_jednokrotnie(), 0)
+        self.assertEqual(ojt.wywolaj_jednokrotnie(), 0)
