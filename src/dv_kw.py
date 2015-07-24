443e462e1b81c9408df836100a9d7f0eb15938e5
Piotr Kasprzyk <ciri@ciri.pl>
2015-07-24 13:00:57 +0200

    Use JednoKrotny

--- src/dv_kw.py
+++ src/dv_kw.py
@@ -3,6 +3,7 @@
 
 NazwyModulow = [wyrazy.split()[1] for wyrazy in '''\
 import sf_kw
+import jk_kw
 '''.splitlines()]
 
 for i in NazwyModulow:
@@ -14,9 +15,15 @@ for i in NazwyModulow:
         else:
             exec 'import %(modul)s' % dict(modul = i)
 
+jedno_krotny = jk_kw.JednoKrotny(use_bool=1)
+
 def zapisz_dla_diagnostyki(odp, wzor):
-    sf_kw.zapisz_plik('a', odp)
-    sf_kw.zapisz_plik('b', wzor)
+    if jedno_krotny.wywolaj_jednokrotnie():
+        sf_kw.zapisz_plik('a', odp)
+        sf_kw.zapisz_plik('b', wzor)
+        raise RuntimeError('Saved to files: a b')
+    else:
+        raise RuntimeError('Skipped - only the first difference was saved.')
 
 def zapisz_ladnie_diagnostyke(odp, wzor):
     sf_kw.zapisz_ladnie('a', odp)
