2803be0649302fd6965703275fca6cdb13d498e7
Piotr Kasprzyk <ciri@ciri.pl>
2015-12-08 06:11:29 +0100

    Use oblicz_moc_jednorazowo

--- src/gt_kw.py
+++ src/gt_kw.py
@@ -97,7 +97,7 @@ class OgOpDaneDlaMiesiaca(object):
         '''
         moja_suma = self.jednorazowe_wartosci.get(tmp_key)
         if moja_suma is None:
-            moja_suma = self.oblicz_jednorazowo(tmp_key)
+            moja_suma = self.oblicz_moc_jednorazowo(tmp_key)
             self.jednorazowe_wartosci[tmp_key] = moja_suma
         return moja_suma
 
