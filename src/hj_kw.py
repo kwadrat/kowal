e0ee8df16a71ea2a50d01d7acc6745383e636418
Piotr Kasprzyk <ciri@ciri.pl>
2016-11-23 12:24:52 +0100

    Remove zeruj_dla_kilku

--- src/hj_kw.py
+++ src/hj_kw.py
@@ -24,13 +24,6 @@ zerowe_pole = db_zero_value + mapping_as
 suffix_comma_separated = '.csv'
 suffix_semicolon_separated = '.txt'
 
-def zeruj_dla_kilku(lista_wzorcowych, tabela_aktualna, nazwa_pola):
-    if tabela_aktualna in lista_wzorcowych:
-        result = zerowe_pole + nazwa_pola
-    else:
-        result = nazwa_pola
-    return result
-
 def zeruj_z_podmiana(dc_changes, tabela_aktualna, nazwa_pola):
     oryg_nazwa = dc_changes.get(tabela_aktualna)
     if oryg_nazwa:
@@ -261,8 +254,6 @@ class TestProcessingSQL(unittest.TestCase):
         self.assertEqual(db_zero_value, '0')
         self.assertEqual(db_strange_value, '44200')
         self.assertEqual(zerowe_pole, '0 AS ')
-        self.assertEqual(zeruj_dla_kilku(['a', 'b'], 'a', 'pole'), '0 AS pole')
-        self.assertEqual(zeruj_dla_kilku(['a', 'b'], 'b', 'pole'), '0 AS pole')
         self.assertEqual(zeruj_z_podmiana({}, 't1', 'pole'), 'pole')
         self.assertEqual(zeruj_z_podmiana({'t1': 'a'}, 't1', 'pole'), 'a AS pole')
         self.assertEqual(zeruj_z_podmiana({'t2': '0'}, 't2', 'field'), '0 AS field')
