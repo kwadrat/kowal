1ea183cf6e9bf9886184f1d02f4ebf092dffa6cc
Piotr Kasprzyk <ciri@ciri.pl>
2018-02-12 14:07:24 +0100

    Rename to xs_semicolon_ql

--- src/civ_kw.py
+++ src/civ_kw.py
@@ -19,7 +19,7 @@ xs_1_ql = ' ORDER BY '
 xs_2_ql = 'UNION\n'
 xs_3_ql = '\n' + xs_2_ql
 xs_4_ql = 'UNION ALL\n'
-xs_5_ql = ';'
+xs_semicolon_ql = ';'
 xs_6_ql = ' DESC'
 
 class TestPartsSQL(unittest.TestCase):
@@ -31,5 +31,5 @@ class TestPartsSQL(unittest.TestCase):
         self.assertEqual(xs_2_ql, 'UNION\n')
         self.assertEqual(xs_3_ql, '\nUNION\n')
         self.assertEqual(xs_4_ql, 'UNION ALL\n')
-        self.assertEqual(xs_5_ql, ';')
+        self.assertEqual(xs_semicolon_ql, ';')
         self.assertEqual(xs_6_ql, ' DESC')
