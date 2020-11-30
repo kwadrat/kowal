cab2f24516a86d7aa11269f2ddda1d29955f0970
Piotr Kasprzyk <ciri@ciri.pl>
2020-11-30 10:25:32 +0100

    Add xs_comma_ql

--- src/civ_kw.py
+++ src/civ_kw.py
@@ -20,6 +20,7 @@ xs_2_ql = 'UNION\n'
 xs_3_ql = '\n' + xs_2_ql
 xs_4_ql = 'UNION ALL\n'
 xs_semicolon_ql = ';'
+xs_comma_ql = ','
 xs_6_ql = ' DESC'
 
 class TestPartsSQL(unittest.TestCase):
@@ -32,4 +33,5 @@ class TestPartsSQL(unittest.TestCase):
         self.assertEqual(xs_3_ql, '\nUNION\n')
         self.assertEqual(xs_4_ql, 'UNION ALL\n')
         self.assertEqual(xs_semicolon_ql, ';')
+        self.assertEqual(xs_comma_ql, ',')
         self.assertEqual(xs_6_ql, ' DESC')
