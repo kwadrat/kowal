a4e88e9c5b3e5cabec4baa0716bd40fbaf9cdaa8
Piotr Kasprzyk <ciri@ciri.pl>
2023-03-24 09:17:10 +0100

    Remove itervalues

--- src/ckc_kw.py
+++ src/ckc_kw.py
@@ -5,15 +5,9 @@ try:
     dict.iteritems
 except AttributeError:
     # Python 3
-    def itervalues(d):
-        return iter(d.values())
-
     def iteritems(d):
         return iter(d.items())
 else:
     # Python 2
-    def itervalues(d):
-        return d.itervalues()
-
     def iteritems(d):
         return d.iteritems()
